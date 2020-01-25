3a9867469f26c41fa4e32b4a0f99ce07be6cced1
Christian Berendt <berendt@betacloud-solutions.de>
2020-01-25 20:18:42 +0100

    contrib: add redis check to check_infrastructure_services.sh

--- contrib/check_infrastructure_services.sh
+++ contrib/check_infrastructure_services.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-packages='libmonitoring-plugin-perl libwww-perl libjson-perl'
+packages='libmonitoring-plugin-perl libwww-perl libjson-perl monitoring-plugins'
 if ! dpkg -s $packages >/dev/null 2>&1; then
   sudo apt-get install -y $packages
 fi
@@ -10,3 +10,5 @@ echo
 echo MariaDB         $(bash nagios-plugins/check_galera_cluster -u root -p qNpdZmkKuUKBK3D5nZ08KMZ5MnYrGEe2hzH6XC0i -H api-int.osism.local)
 echo
 echo RabbitMQ        $(perl nagios-plugins/check_rabbitmq_cluster -H api-int.osism.local -u openstack -p BO6yGAAq9eqA7IKqeBdtAEO7aJuNu4zfbhtnRo8Y)
+echo
+echo Redis           $(/usr/lib/nagios/plugins/check_tcp -H 192.168.50.10 -p 6379 -A -E -s 'AUTH QHNA1SZRlOKzLADhUd5ZDgpHfQe6dNfr3bwEdY24\r\nPING\r\nINFO replication\r\nQUIT\r\n' -e 'PONG' -e 'role:master' -e 'slave0:ip=192.168.50.11,port=6379' -j)
