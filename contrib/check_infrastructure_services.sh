d314fee6961eca4c1c6ebc6beb05961abe2101f9
Christian Berendt <berendt@betacloud-solutions.de>
2020-01-25 16:09:47 +0100

    contrib: install required packages in check_infrastructure_services.sh

--- contrib/check_infrastructure_services.sh
+++ contrib/check_infrastructure_services.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-echo Elasticsearch
-echo
-bash nagios-plugins/check_elasticsearch -H api-int.osism.local
+packages='libmonitoring-plugin-perl libwww-perl libjson-perl'
+if ! dpkg -s $packages >/dev/null 2>&1; then
+  sudo apt-get install -y $packages
+fi
 
+echo Elasticsearch   $(bash nagios-plugins/check_elasticsearch -H api-int.osism.local)
 echo
-echo MariaDB
+echo MariaDB         $(bash nagios-plugins/check_galera_cluster -u root -p qNpdZmkKuUKBK3D5nZ08KMZ5MnYrGEe2hzH6XC0i -H api-int.osism.local)
 echo
-bash nagios-plugins/check_galera_cluster -u root -p qNpdZmkKuUKBK3D5nZ08KMZ5MnYrGEe2hzH6XC0i -H api-int.osism.local
-
-echo
-echo RabbitMQ
-echo
-perl nagios-plugins/check_rabbitmq_cluster -H api-int.osism.local -u openstack -p BO6yGAAq9eqA7IKqeBdtAEO7aJuNu4zfbhtnRo8Y
+echo RabbitMQ        $(perl nagios-plugins/check_rabbitmq_cluster -H api-int.osism.local -u openstack -p BO6yGAAq9eqA7IKqeBdtAEO7aJuNu4zfbhtnRo8Y)
