0ef4273925ba500bdf0c4b0bf8556f5bd6f1c70e
Christian Berendt <berendt@betacloud-solutions.de>
2020-01-26 17:29:21 +0100

    Deploy nova & neutron

--- scripts/deploy_openstack_services.sh
+++ scripts/deploy_openstack_services.sh
@@ -6,4 +6,6 @@ osism-kolla deploy keystone
 osism-kolla deploy horizon
 osism-kolla deploy glance
 osism-kolla deploy cinder
+osism-kolla deploy neutron
+osism-kolla deploy nova
 osism-kolla deploy heat
