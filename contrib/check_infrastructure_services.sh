0a3e8d09f919d1e669ff8db6cdab223c948083c1
Christian Berendt <berendt@betacloud-solutions.de>
2020-01-26 15:45:20 +0100

    contrib: wsrep size of 2 is fine in the testbed

--- contrib/check_infrastructure_services.sh
+++ contrib/check_infrastructure_services.sh
@@ -7,7 +7,7 @@ fi
 
 echo "Elasticsearch   $(bash nagios-plugins/check_elasticsearch -H api-int.osism.local)"
 echo
-echo "MariaDB         $(bash nagios-plugins/check_galera_cluster -u root -p qNpdZmkKuUKBK3D5nZ08KMZ5MnYrGEe2hzH6XC0i -H api-int.osism.local)"
+echo "MariaDB         $(bash nagios-plugins/check_galera_cluster -u root -p qNpdZmkKuUKBK3D5nZ08KMZ5MnYrGEe2hzH6XC0i -H api-int.osism.local -c 1)"
 echo
 echo "RabbitMQ        $(perl nagios-plugins/check_rabbitmq_cluster -H api-int.osism.local -u openstack -p BO6yGAAq9eqA7IKqeBdtAEO7aJuNu4zfbhtnRo8Y)"
 echo
