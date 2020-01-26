8e025da4459f28fe5871f54f994c5a5400e42fb8
Christian Berendt <berendt@betacloud-solutions.de>
2020-01-26 18:03:22 +0100

    Add script to check openstack services

---
+++ contrib/check_openstack_services.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+openstack --os-cloud admin endpoint list
+
+echo
+echo "# Heat"
+echo
+
+openstack --os-cloud admin orchestration service list
+
+echo
+echo "# Cinder"
+echo
+
+openstack --os-cloud admin volume service list
+
+echo
+echo "# Neutron"
+echo
+
+openstack --os-cloud admin network agent list
+openstack --os-cloud admin network service provider list
+
+echo
+echo "# Nova"
+echo
+
+openstack --os-cloud admin compute service list
+openstack --os-cloud admin hypervisor list
