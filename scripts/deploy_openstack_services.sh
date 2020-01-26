3cb2823c74e2f0a76a8c783715244b0c2f0170e1
Christian Berendt <berendt@betacloud-solutions.de>
2020-01-26 21:24:34 +0100

    Enable telemetry stack
    Closes #7

--- scripts/deploy_openstack_services.sh
+++ scripts/deploy_openstack_services.sh
@@ -9,3 +9,7 @@ osism-kolla deploy cinder
 osism-kolla deploy neutron
 osism-kolla deploy nova
 osism-kolla deploy heat
+osism-kolla deploy gnocchi
+osism-kolla deploy ceilometer
+osism-kolla deploy aodh
+osism-kolla deploy panko
