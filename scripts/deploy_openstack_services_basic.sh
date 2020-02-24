2420361641dffce83366c5622c97b4bc839f85aa
Christian Berendt <berendt@betacloud-solutions.de>
2020-02-24 18:09:05 +0100

    scripts: split the deploy_openstack_services.sh

---
+++ scripts/deploy_openstack_services_basic.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export INTERACTIVE=false
+
+# deploy basic OpenStack services
+
+osism-kolla deploy keystone
+osism-kolla deploy horizon
+osism-kolla deploy placement
+osism-kolla deploy glance
+osism-kolla deploy cinder
+osism-kolla deploy neutron
+osism-kolla deploy nova
+
+osism-custom run bootstrap-basic
