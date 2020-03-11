4babd019b8a99493cdda97dcdb4ba9aa851271ff
Christian Berendt <berendt@betacloud-solutions.de>
2020-03-11 23:16:04 +0100

    Drop kibana workaround

--- scripts/deploy_infrastructure_services.sh
+++ scripts/deploy_infrastructure_services.sh
@@ -15,10 +15,5 @@ osism-kolla deploy haproxy
 osism-kolla deploy elasticsearch
 osism-kolla deploy kibana
 osism-kolla deploy etcd
-
-# NOTE: workaround "Index .kibana belongs to a version of Kibana that cannot be
-#       automatically migrated. Reset it or use the X-Pack upgrade assistant."
-curl -X DELETE http://192.168.50.200:9200/.kibana
-
 osism-kolla deploy rabbitmq
 osism-kolla deploy mariadb
