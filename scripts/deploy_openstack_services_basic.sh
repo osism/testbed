01cf3e4e3a348e1c726d8855982841aede62a910
Christian Berendt <berendt@betacloud-solutions.de>
2020-03-12 11:36:50 +0100

    Use kolla testbed playbook to deploy infra + openstack base (#83)

--- scripts/deploy_openstack_services_basic.sh
+++ scripts/deploy_openstack_services_basic.sh
@@ -4,12 +4,5 @@ export INTERACTIVE=false
 
 # deploy basic OpenStack services
 
-osism-kolla deploy keystone
-osism-kolla deploy horizon
-osism-kolla deploy placement
-osism-kolla deploy glance
-osism-kolla deploy cinder
-osism-kolla deploy neutron
-osism-kolla deploy nova
-
+osism-kolla deploy testbed --tags openstack
 osism-run openstack bootstrap-basic
