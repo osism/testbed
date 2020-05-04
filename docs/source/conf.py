8b23d40dcc47a7e70c5d6f1bf70ea37f296727b5
Christian Berendt <berendt@betacloud-solutions.de>
2020-05-04 10:56:45 +0200

    Add sphinx documentation
    Closes #153

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+def setup(app):
+    app.add_stylesheet('css/custom.css')
+
+
+extensions = [
+  'sphinx.ext.autosectionlabel', 'sphinxcontrib.blockdiag',
+  'sphinxcontrib.nwdiag', 'sphinx.ext.todo', 'sphinx_fontawesome'
+  ]
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'OSISM Testbed'
+copyright = u'2019-2020, Betacloud Solutions GmbH'
+author = u'Betacloud Solutions GmbH'
+version = u''
+release = u''
+language = 'en'
+exclude_patterns = []
+pygments_style = 'sphinx'
+todo_include_todos = True
+html_theme = 'sphinx_rtd_theme'
+html_show_sphinx = False
+html_show_sourcelink = False
+html_show_copyright = True
+htmlhelp_basename = 'documentation'
+html_theme_options = {
+    'display_version': False,
+    'canonical_url': 'https://docs.osism.de/testbed/',
+    'style_external_links': True,
+    'logo_only': True,
+    'prev_next_buttons_location': None
+}
+html_context = {
+    'display_github': True,
+    'github_user': 'osism',
+    'github_repo': 'documentation',
+    'github_version': 'master',
+    'conf_py_path': '/source/'
+}
+html_logo = 'images/logo.png'
+html_static_path = [
+    '_static'
+]
+latex_elements = {}
