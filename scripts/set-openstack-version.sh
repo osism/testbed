092bcef81cc2af83e3651bfd48f03a5b677d114d
Christian Berendt <berendt@betacloud-solutions.de>
2020-06-08 17:07:30 +0200

    Fix "do not replace train in constraint" (#239)

--- scripts/set-openstack-version.sh
+++ scripts/set-openstack-version.sh
@@ -3,4 +3,4 @@
 DEFAULT_VERSION=train
 VERSION=${1:-train}
 
-grep -rlZ $DEFAULT_VERSION * | grep -v constraint | xargs -0 sed -i "s/${DEFAULT_VERSION}/${VERSION}/g"
+grep -rlZ "$DEFAULT_VERSION" * | xargs -0 sed -i "/constraint/! s/${DEFAULT_VERSION}/${VERSION}/g"
