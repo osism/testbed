33c9e3ce5a2d0afb785061431f9803822296009a
Christian Berendt <berendt@betacloud-solutions.de>
2020-06-30 14:32:51 +0200

    Fix broken refstack tests (#191)
    Closes #146

--- contrib/refstack/refstack.sh
+++ contrib/refstack/refstack.sh
@@ -3,10 +3,20 @@
 INTERACTIVE=false osism-run openstack bootstrap-basic
 INTERACTIVE=false osism-run openstack bootstrap-refstack
 
+# NOTE: create RGW user accounts
+for username in refstack-0 refstack-1 refstack-2; do
+    openstack --os-cloud $username container list
+done
+
+# NOTE: enable quota management on RGW user accounts
+for username in $(radosgw-admin user list | grep \\$ | awk -F\" '{ print $2 }'); do
+    radosgw-admin quota enable --uid "$username" --quota-scope=user
+done
+
 sudo mkdir -p /opt/refstack
 sudo chown dragon: /opt/refstack
 
-git clone https://opendev.org/osf/refstack-client /opt/refstack/client
+git clone https://opendev.org/osf/refstack-client.git /opt/refstack/client
 
 pushd /opt/refstack/client
 ./setup_env
@@ -18,9 +28,7 @@ TARGET=${2:-platform}
 # NOTE: AccountQuotasNegativeTest.test_user_modify_quota and AccountQuotasTest.test_upload_valid_object
 #       are not working with Ceph RGW
 
-curl -s "https://refstack.openstack.org/api/v1/guidelines/$GUIDELINE/tests?target=$TARGET&type=required,advisory&alias=true&flag=true" | \
-    grep -v tempest.api.object_storage.test_account_quotas_negative.AccountQuotasNegativeTest.test_user_modify_quota | \
-    grep -v tempest.api.object_storage.test_account_quotas.AccountQuotasTest.test_upload_valid_object \
+curl -s "https://refstack.openstack.org/api/v1/guidelines/$GUIDELINE/tests?target=$TARGET&type=required,advisory&alias=true&flag=true" \
     > /opt/refstack/test-list.txt
 
 source /opt/refstack/client/.venv/bin/activate
