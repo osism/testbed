0010fb7b0e02c9461b87fa63d95d80a28fbfc783
Christian Berendt <berendt@betacloud-solutions.de>
2020-06-30 17:11:27 +0200

    Deploy osquery service (#258)

--- scripts/deploy_infrastructure_services.sh
+++ scripts/deploy_infrastructure_services.sh
@@ -5,6 +5,7 @@ export INTERACTIVE=false
 osism-infrastructure helper --tags phpmyadmin
 osism-infrastructure openstackclient
 osism-infrastructure patchman
+osism-generic osquery
 osism-generic patchman-client
 osism-generic cockpit
 
