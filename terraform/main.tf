770db211bb9fa81780ad8f1944209cd52d521225
Christian Berendt <berendt@betacloud-solutions.de>
2020-07-09 13:41:57 +0200

    terraform: add workspaces
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,4 +4,9 @@ provider "openstack" {
 
 terraform {
   required_version = ">= 0.12"
+
+  backend "local" {
+    path          = "terraform.tfstate"
+    workspace_dir = "terraform-workspace-${var.cloud_provider}"
+  }
 }
