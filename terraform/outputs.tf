b350a758764dc8ffdf4a5582b781b22217c3e969
Christian Berendt <berendt@betacloud-solutions.de>
2020-07-09 16:05:50 +0200

    terraform: fix output filenames
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,13 +7,13 @@ output "private_key" {
 }
 
 resource "local_file" "id_rsa" {
-  filename          = ".id_rsa.testbed"
+  filename          = ".id_rsa.${var.cloud_provider}"
   file_permission   = "0600"
   sensitive_content = openstack_compute_keypair_v2.key.private_key
 }
 
 resource "local_file" "MANAGER_ADDRESS" {
-  filename        = ".MANAGER_ADDRESS.testbed"
+  filename        = ".MANAGER_ADDRESS.${var.cloud_provider}"
   file_permission = "0644"
   content         = "MANAGER_ADDRESS=${openstack_networking_floatingip_v2.manager_floating_ip.address}\n"
 }
