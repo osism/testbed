ec3a54ed75d3360e69ce65222bb60cb78092c456
Christian Berendt <berendt@betacloud-solutions.de>
2020-07-11 16:52:24 +0200

    custom: add playbook to bootstrap patchman
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- scripts/deploy_infrastructure_services.sh
+++ scripts/deploy_infrastructure_services.sh
@@ -4,9 +4,11 @@ export INTERACTIVE=false
 
 osism-infrastructure helper --tags phpmyadmin
 osism-infrastructure openstackclient
-osism-infrastructure patchman
 osism-generic osquery
-osism-generic patchman-client
 osism-generic cockpit
+
+osism-infrastructure patchman
+osism-generic patchman-client
+osism-run custom bootstrap-patchman
 
 osism-kolla deploy testbed --tags infrastructure
