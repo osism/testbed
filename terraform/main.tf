c1cdc86807c8ba4092113f961b5c153ff8a0fc71
Christian Berendt <berendt@betacloud-solutions.de>
2020-08-14 20:50:27 +0200

    github: test terraform 0.12.29 & 0.13.0
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,4 +4,23 @@ provider "openstack" {
 
 terraform {
   required_version = ">= 0.12"
+
+  # The "hashicorp" namespace is the new home for the HashiCorp-maintained
+  # provider plugins.
+  #
+  # source is not required for the hashicorp/* namespace as a measure of
+  # backward compatibility for commonly-used providers, but recommended for
+  # explicitness.
+  #
+  # source is required for providers in other namespaces, to avoid ambiguity.
+
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+
+    openstack = {
+      source = "terraform-providers/openstack"
+    }
+  }
 }
