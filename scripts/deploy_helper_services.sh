86e6630b4415b6fa2180cb019bb320557be72e9f
Christian Berendt <berendt@betacloud-solutions.de>
2020-08-15 17:59:43 +0200

    Use 'osism-manager sshconfig' to deploy sshconfig
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- scripts/deploy_helper_services.sh
+++ scripts/deploy_helper_services.sh
@@ -2,7 +2,7 @@
 
 export INTERACTIVE=false
 
-osism-infrastructure helper --tags sshconfig
+osism-manager sshconfig
 osism-run custom generate-ssh-known-hosts
 osism-run custom wireguard
 osism-generic dotfiles
