fe35969c829e8b42030e592d38534dded73cd17b
Christian Berendt <berendt@betacloud-solutions.de>
2020-11-11 18:23:43 +0100

    inventory: calculate node IP addresses (#457)
    Closes #421
    
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- terraform/files/node.sh
+++ terraform/files/node.sh
@@ -10,9 +10,10 @@ python3 /root/configure-network-devices.py
 chown -R ubuntu:ubuntu /home/ubuntu/.ssh
 
 add-apt-repository --yes ppa:ansible/ansible
-apt-get install --yes ansible
+apt-get install --yes ansible python-netaddr
 
 ansible-galaxy install git+https://github.com/osism/ansible-docker
+ansible-galaxy collection install ansible.netcommon
 
 git clone https://github.com/osism/ansible-collection-commons.git /tmp/ansible-collection-commons
 ( cd /tmp/ansible-collection-commons; ansible-galaxy collection build; ansible-galaxy collection install -v -f -p /usr/share/ansible/collections osism-commons-*.tar.gz; )
