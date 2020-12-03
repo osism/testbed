bf47163d23e4beb99e0804c35b41e93bbffa418d
Christian Berendt <berendt@betacloud-solutions.de>
2020-12-03 10:22:44 +0100

    terraform: remove redundance from the manager.sh script (#527)
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- terraform/manager.tf
+++ terraform/manager.tf
@@ -158,16 +158,6 @@ write_files:
       cp /home/ubuntu/.ssh/id_rsa.pub /home/dragon/.ssh/id_rsa.pub
       chown -R dragon:dragon /home/dragon/.ssh
 
-      git clone https://github.com/osism/ansible-collection-commons.git /tmp/ansible-collection-commons
-      ( cd /tmp/ansible-collection-commons; ansible-galaxy collection build; sudo -iu dragon ansible-galaxy collection install -v -f -p /usr/share/ansible/collections osism-commons-*.tar.gz; )
-      rm -rf /tmp/ansible-collection-commons
-
-      git clone https://github.com/osism/ansible-collection-services.git /tmp/ansible-collection-services
-      ( cd /tmp/ansible-collection-services; ansible-galaxy collection build; sudo -iu dragon ansible-galaxy collection install -v -f -p /usr/share/ansible/collections osism-services-*.tar.gz; )
-      rm -rf /tmp/ansible-collection-services
-
-      sudo -iu dragon ansible-galaxy collection install ansible.netcommon
-
       sudo -iu dragon ansible-playbook -i testbed-manager.osism.test, /opt/manager-part-1.yml -e configuration_git_version=${var.configuration_version}
       sudo -iu dragon sh -c 'cd /opt/configuration; ./scripts/set-ceph-version.sh ${var.ceph_version}'
       sudo -iu dragon sh -c 'cd /opt/configuration; ./scripts/set-openstack-version.sh ${var.openstack_version}'
