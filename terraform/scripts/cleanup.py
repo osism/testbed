b80374fc166393e415c474a7271cd8ac34a541a1
Kurt Garloff <kurt@garloff.de>
2021-02-08 12:18:53 +0100

    Improve cleanup script (#604)
    * Also cleanup keypairs.
    * Also remove volumes.
    * Collect complete list prior to deleting elements:
      We otherwise risk to miss elements due to unstable sorting (esp. with
      pagination)
    * User server-side filtering for ports and floating IPs for efficiency
    * We can pass the objects directly rather than the IDs when we delete
      them
    * Disconnect interfaces from router prior to deleting subnets and nets
    * Delete subnets (prior to nets)
    * Wait for VMs to be gone before deleting ports and volumes
    * Delete router last (this is a special OTC requirement)
    * Use server-side filtering where possible.
    
    This is a performance speedup:
    * Filter only detach floating IPs
    * Filter server and volume names that start with testbed
      (cinder and nova support regexp filtering)
    * Only look for networks that are not shared
    * Only look for ports that are DOWN (this was there before)

--- terraform/scripts/cleanup.py
+++ terraform/scripts/cleanup.py
@@ -2,6 +2,7 @@
 
 import logging
 import os
+import time
 
 import openstack
 
@@ -9,85 +10,129 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 
-def cleanup_routers(conn):
-    logging.info("clean up routers")
-    for router in conn.network.routers():
+def disconnect_routers(conn):
+    logging.info("disconnect routers")
+    routers = list(conn.network.routers())
+    for router in routers:
         router_dict = router.to_dict()
         router_name = router_dict["name"]
-
+        router_id = router_dict["id"]
         if not router_name.startswith("testbed"):
             continue
 
         logging.info(router_name)
-
-        conn.network.remove_gateway_from_router(router_dict["id"])
-
-        for port in conn.network.ports(device_id=router_dict["id"]):
-            conn.network.remove_interface_from_router(router_dict["id"],
+        ports = list(conn.network.ports(device_id=router_id))
+        for port in ports:
+            conn.network.remove_interface_from_router(router_id,
                                                       port_id=port["id"])
 
-        conn.network.delete_router(router_dict["id"])
+
+def cleanup_routers(conn):
+    logging.info("clean up routers")
+    routers = list(conn.network.routers())
+    for router in routers:
+        router_name = router.to_dict()["name"]
+        if not router_name.startswith("testbed"):
+            continue
+
+        logging.info(router_name)
+        conn.network.remove_gateway_from_router(router)
+        conn.network.delete_router(router)
 
 
 def cleanup_networks(conn):
     logging.info("clean up networks")
-    for network in conn.network.networks():
-        network_dict = network.to_dict()
-        network_name = network_dict["name"]
-
+    networks = list(conn.network.networks(shared=False))
+    for network in networks:
+        network_name = network.to_dict()["name"]
         if not network_name.startswith("net-testbed"):
             continue
 
         logging.info(network_name)
-        conn.network.delete_network(network_dict["id"])
+        conn.network.delete_network(network)
+
+
+def cleanup_subnets(conn):
+    logging.info("clean up subnets")
+    subnets = list(conn.network.subnets())
+    for subnet in subnets:
+        subnet_name = subnet.to_dict()["name"]
+        if not subnet_name.startswith("subnet-testbed"):
+            continue
+
+        logging.info(subnet_name)
+        conn.network.delete_subnet(subnet)
 
 
 def cleanup_ports(conn):
     logging.info("clean up ports")
-    for port in conn.network.ports():
+    # FIXME: We can't filter for device_owner = '' unfortunately
+    ports = list(conn.network.ports(status="DOWN"))
+    for port in ports:
         port_dict = port.to_dict()
-        port_status = port_dict["status"]
-        port_device_owner = port_dict["device_owner"]
-
-        if not port_status == "DOWN" or port_device_owner != "":
+        assert(port_dict["status"] == "DOWN")
+        if (port_dict["device_owner"] != ""):
             continue
 
         logging.info(port_dict["id"])
-        conn.network.delete_port(port_dict["id"])
+        conn.network.delete_port(port)
 
 
 def cleanup_volumes(conn):
     logging.info("clean up volumes")
-    for volume in conn.block_storage.volumes():
-        volume_dict = volume.to_dict()
-        volume_name = volume_dict["name"]
-
+    # cinder supports regex filtering
+    volumes = list(conn.block_storage.volumes(name="^testbed"))
+    for volume in volumes:
+        volume_name = volume.to_dict()["name"]
         if not volume_name.startswith("testbed"):
             continue
 
         logging.info(volume_name)
-        conn.block_storage.delete_volume(volume_dict["id"])
+        conn.block_storage.delete_volume(volume)
 
 
 def cleanup_servers(conn):
     logging.info("clean up servers")
-    for server in conn.compute.servers():
-        server_dict = server.to_dict()
-        server_name = server_dict["name"]
-
+    # nova supports regex filtering
+    servers = list(conn.compute.servers(name="^testbed"))
+    for server in servers:
+        server_name = server.to_dict()["name"]
         if not server_name.startswith("testbed"):
             continue
 
         logging.info(server_name)
-        conn.compute.delete_server(server_dict["id"], force=True)
+        try:
+            conn.compute.delete_server(server, force=True)
+        except openstack.exceptions.HttpException:
+            conn.compute.delete_server(server)
+
+
+def wait_servers_gone(conn):
+    logging.info("wait for servers to be gone")
+    count = 0
+    found = []
+    while count < 100:
+        found = []
+        # nova supports regex filtering
+        servers = list(conn.compute.servers(name="^testbed"))
+        for server in servers:
+            server_name = server.to_dict()["name"]
+            if server_name.startswith("testbed"):
+                found.append(server_name)
+        if not found:
+            break
+        count += 1
+        time.sleep(2)
+
+    if count >= 100:
+        logging.error("timeout waiting for servers to vanish: %s" % found)
 
 
 def cleanup_keypairs(conn):
     logging.info("clean up keypairs")
-    for keypair in conn.compute.keypairs():
-        keypair_dict = keypair.to_dict()
-        keypair_name = keypair_dict["name"]
-
+    keypairs = list(conn.compute.keypairs())
+    for keypair in keypairs:
+        keypair_name = keypair.to_dict()["name"]
         if not keypair_name.startswith("testbed"):
             continue
 
@@ -98,9 +143,7 @@ def cleanup_keypairs(conn):
 def cleanup_security_groups(conn):
     logging.info("clean up security groups")
     for security_group in conn.network.security_groups():
-        security_group_dict = security_group.to_dict()
-        security_group_name = security_group_dict["name"]
-
+        security_group_name = security_group.to_dict()["name"]
         if not security_group_name.startswith("testbed"):
             continue
 
@@ -109,26 +152,39 @@ def cleanup_security_groups(conn):
 
 
 def cleanup_floating_ips(conn):
+    # Note: FIPs have no name, so we might clean up unrelated
+    #  currently unused FIPs here.
     logging.info("clean up floating ips")
-    for floating_ip in conn.search_floating_ips():
+    floating_ips = list(conn.search_floating_ips(filters={"attached": False}))
+    for floating_ip in floating_ips:
         floating_ip_dict = dict(floating_ip)
         floating_ip_name = floating_ip["floating_ip_address"]
-
-        if not floating_ip_dict["attached"]:
-            logging.info(floating_ip_name)
-            conn.delete_floating_ip(floating_ip_dict["id"])
+        assert(not floating_ip_dict["attached"])
+        logging.info(floating_ip_name)
+        conn.delete_floating_ip(floating_ip_dict["id"])
 
 
 def main():
-    conn = openstack.connect(cloud=os.environ['ENVIRONMENT'])
+    try:
+        OSENV = os.environ['OS_CLOUD']
+    except KeyError:
+        try:
+            OSENV = os.environ['ENVIRONMENT']
+        except KeyError as e:
+            logging.error("Need to have OS_CLOUD or ENVIRONMENT set!")
+            raise(e)
+    conn = openstack.connect(cloud=OSENV)
     cleanup_servers(conn)
-    cleanup_volumes(conn)
-    cleanup_ports(conn)
-    cleanup_routers(conn)
-    cleanup_networks(conn)
-    cleanup_keypairs(conn)
-    cleanup_security_groups(conn)
     cleanup_floating_ips(conn)
+    cleanup_keypairs(conn)
+    wait_servers_gone(conn)
+    cleanup_ports(conn)
+    cleanup_volumes(conn)
+    disconnect_routers(conn)
+    cleanup_subnets(conn)
+    cleanup_networks(conn)
+    cleanup_security_groups(conn)
+    cleanup_routers(conn)
 
 
 if __name__ == '__main__':
