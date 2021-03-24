b236940df9c21962952956ce24e95493ea60ebe9
Christian Berendt <berendt@betacloud-solutions.de>
2021-03-24 16:56:11 +0100

    openstack: use octavia bootstrap process integrated in kolla-ansible (#631)
    Closes #571
    
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- scripts/004-openstack-services-basic.sh
+++ scripts/004-openstack-services-basic.sh
@@ -4,10 +4,7 @@ export INTERACTIVE=false
 
 osism-kolla deploy testbed --tags openstack --skip-tags infrastructure
 
-osism-run openstack bootstrap-octavia-pre
 osism-kolla deploy octavia
-osism-run openstack bootstrap-octavia-post
-
 osism-kolla deploy barbican
 osism-kolla deploy designate
 
