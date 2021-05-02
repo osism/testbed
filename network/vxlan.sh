b5056321b6bda240cda2cb77a974a4a8c266b31d
Christian Berendt <berendt@betacloud-solutions.de>
2021-05-02 19:25:43 +0200

    Use netplan for the network configuration of the manager (#683)
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

---
+++ network/vxlan.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [[ $IFACE == "{{ ansible_local.testbed_network_devices.management }}" ]]; then
+    ip link add vxlan0 type vxlan id 42 group 239.1.1.1 dstport 4789 dev {{ ansible_local.testbed_network_devices.management }}
+    ip addr add {{ '192.168.112.0/20' | ipaddr('net') | ipaddr('5') | ipaddr('address') }}/20 dev vxlan0
+    ip link set up dev vxlan0
+fi
