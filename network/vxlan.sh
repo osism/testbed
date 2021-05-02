1fd692b234ec733024a8baf913ffa21c52786980
Christian Berendt <berendt@betacloud-solutions.de>
2021-05-02 21:03:12 +0200

    inventory: use node_id parameter (#685)
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- network/vxlan.sh
+++ network/vxlan.sh
@@ -2,6 +2,6 @@
 
 if [[ $IFACE == "{{ ansible_local.testbed_network_devices.management }}" ]]; then
     ip link add vxlan0 type vxlan id 42 group 239.1.1.1 dstport 4789 dev {{ ansible_local.testbed_network_devices.management }}
-    ip addr add {{ '192.168.112.0/20' | ipaddr('net') | ipaddr('5') | ipaddr('address') }}/20 dev vxlan0
+    ip addr add {{ '192.168.112.0/20' | ipaddr('net') | ipaddr(node_id) | ipaddr('address') }}/20 dev vxlan0
     ip link set up dev vxlan0
 fi
