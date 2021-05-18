8a41bb65054f50f8bd2aa21fa27cf60669a38af3
Christian Berendt <berendt@betacloud-solutions.de>
2021-05-18 09:24:07 +0200

    terraform: add a check target to the Makefile
    Partial #716
    
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- contrib/check_infrastructure_services.sh
+++ contrib/check_infrastructure_services.sh
@@ -2,13 +2,33 @@
 
 packages='libmonitoring-plugin-perl libwww-perl libjson-perl monitoring-plugins mysql-client'
 if ! dpkg -s $packages >/dev/null 2>&1; then
-  sudo apt-get install -y $packages
+  sudo apt-get install -y $packages >/dev/null 2>&1
 fi
 
-echo "Elasticsearch   $(bash nagios-plugins/check_elasticsearch -H api-int.osism.test)"
+pushd /opt/configuration/contrib > /dev/null
+
 echo
-echo "MariaDB         $(bash nagios-plugins/check_galera_cluster -u root -p qNpdZmkKuUKBK3D5nZ08KMZ5MnYrGEe2hzH6XC0i -H api-int.osism.test -c 1)"
+echo "# Elasticsearch"
 echo
-echo "RabbitMQ        $(perl nagios-plugins/check_rabbitmq_cluster -H api-int.osism.test -u openstack -p BO6yGAAq9eqA7IKqeBdtAEO7aJuNu4zfbhtnRo8Y)"
+
+bash nagios-plugins/check_elasticsearch -H api-int.osism.test
+
 echo
-echo "Redis           $(/usr/lib/nagios/plugins/check_tcp -H 192.168.16.10 -p 6379 -A -E -s 'AUTH QHNA1SZRlOKzLADhUd5ZDgpHfQe6dNfr3bwEdY24\r\nPING\r\nINFO replication\r\nQUIT\r\n' -e 'PONG' -e 'role:master' -e 'slave0:ip=192.168.16.11,port=6379' -j)"
+echo "# MariaDB"
+echo
+
+bash nagios-plugins/check_galera_cluster -u root -p qNpdZmkKuUKBK3D5nZ08KMZ5MnYrGEe2hzH6XC0i -H api-int.osism.test -c 1
+
+echo
+echo "# RabbitMQ"
+echo
+
+perl nagios-plugins/check_rabbitmq_cluster -H api-int.osism.test -u openstack -p BO6yGAAq9eqA7IKqeBdtAEO7aJuNu4zfbhtnRo8Y
+
+echo
+echo "# Redis"
+echo
+
+/usr/lib/nagios/plugins/check_tcp -H 192.168.16.10 -p 6379 -A -E -s 'AUTH QHNA1SZRlOKzLADhUd5ZDgpHfQe6dNfr3bwEdY24\r\nPING\r\nINFO replication\r\nQUIT\r\n' -e 'PONG' -e 'role:master' -e 'slave0:ip=192.168.16.11,port=6379' -j
+
+popd > /dev/null
