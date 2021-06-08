7f6a5e502397acf80cb8cb948cec429557cbdfd2
Ferenc Géczi <geczi@univention.de>
2021-06-08 00:00:00 +0000

    feat(keycloak): "osism-run custom keycloak-ldap-federation-config" in identity-services.sh
    Signed-off-by: Ferenc Géczi <geczi@univention.de>

--- scripts/999-identity-services.sh
+++ scripts/999-identity-services.sh
@@ -5,4 +5,5 @@ export INTERACTIVE=false
 osism-infrastructure openstackclient
 osism-infrastructure keycloak
 osism-run custom keycloak-oidc-client-config
+osism-run custom keycloak-ldap-federation-config
 osism-kolla deploy testbed-identity
