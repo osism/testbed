622dc1de66418da310440996bc81b2c56efd60e1
Christian Berendt <berendt@osism.tech>
2021-06-28 13:01:46 +0200

    Use openstack wallaby release as default
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/set-openstack-version.sh
+++ scripts/set-openstack-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DEFAULT_VERSION=victoria
-VERSION=${1:-victoria}
+DEFAULT_VERSION=wallaby
+VERSION=${1:-wallaby}
 
 grep -rlZ "$DEFAULT_VERSION" * | xargs -0 sed -i "/constraint/! s/${DEFAULT_VERSION}/${VERSION}/g"
