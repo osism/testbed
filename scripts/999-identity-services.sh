fdb4c3c13a0b74b4254324cba36e570e1edac2db
Ferenc- <Ferenc-@users.noreply.github.com>
2021-09-13 12:33:05 +0200

    custom: use a separate umc-admin LDAP user with admin privileges (#825)
    Signed-off-by: Ferenc Géczi <geczi@univention.de>

--- scripts/999-identity-services.sh
+++ scripts/999-identity-services.sh
@@ -5,6 +5,8 @@ export INTERACTIVE=false
 osism-infrastructure openstackclient
 osism-infrastructure keycloak
 osism-infrastructure openldap
+osism-run custom openldap-umc-policies
+osism-run custom umc-admin-ldap
 osism-run custom keycloak-oidc-client-config
 osism-run custom keycloak-ldap-federation-config
 osism-kolla deploy testbed-identity
