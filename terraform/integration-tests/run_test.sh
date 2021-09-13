671327166994225269bdb03025a4ca4fce5298b3
Ferenc- <Ferenc-@users.noreply.github.com>
2021-09-13 12:32:48 +0200

    Test login to Horizon via Keycloak LDAP federation (#797)
    Signed-off-by: Ferenc Géczi <geczi@univention.de>

---
+++ terraform/integration-tests/run_test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Pull often fails with "net/http: TLS handshake timeout"
+until docker-compose pull; do
+    echo "Failed to pull images, retrying"
+done
+
+docker-compose up --no-build --remove-orphans --exit-code-from test
+docker-compose rm -f
+
+ls -lah robot_results
