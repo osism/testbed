ac87f528964cc48933aeda94077c42073aedc10a
Christian Berendt <berendt@osism.tech>
2021-11-16 19:39:21 +0100

    Enforce the run of patchman-client (#932)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/006-infrastructure-services-extented.sh
+++ scripts/006-infrastructure-services-extented.sh
@@ -5,7 +5,7 @@ export INTERACTIVE=false
 osism-infrastructure phpmyadmin
 
 osism-infrastructure patchman
-osism-generic patchman-client
+osism-generic patchman-client -e patchman_client_update_force=true
 
 # NOTE: After all clients have transferred their data with the
 #       previous call, the evaluation of this data is now triggered.
