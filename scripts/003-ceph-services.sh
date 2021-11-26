f7fe4917144f9b1af887b633c3abe06541db2559
Dr. Jens Harbott <harbott@osism.tech>
2021-11-26 12:27:33 +0100

    Fix ceph status warning
    Signed-off-by: Dr. Jens Harbott <harbott@osism.tech>

--- scripts/003-ceph-services.sh
+++ scripts/003-ceph-services.sh
@@ -7,3 +7,5 @@ osism-ceph rgws
 osism-run custom fetch-ceph-keys
 osism-infrastructure cephclient
 osism-run custom bootstrap-ceph-dashboard
+ceph config set mon auth_allow_insecure_global_id_reclaim false
+
