429a2bdf4a6f400ae9492d05c0c5c506c0115d41
Christian Berendt <berendt@osism.tech>
2021-11-26 22:26:04 +0100

    The virtualbmc service requires docker-compose
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/009-openstack-services-baremetal.sh
+++ scripts/009-openstack-services-baremetal.sh
@@ -5,5 +5,10 @@ export INTERACTIVE=false
 curl https://tarballs.opendev.org/openstack/ironic-python-agent/dib/files/ipa-centos8-stable-wallaby.kernel -o /opt/configuration/environments/kolla/files/overlays/ironic/ironic-agent.kernel
 curl https://tarballs.opendev.org/openstack/ironic-python-agent/dib/files/ipa-centos8-stable-wallaby.initramfs -o /opt/configuration/environments/kolla/files/overlays/ironic/ironic-agent.initramfs
 
+# NOTE: The docker-compose role is currently required for the
+#       virtualbmc service. Can be removed again when everything
+#       has been switched to the docker compose cli plugin.
+osism-generic docker-compose
+
 osism-infrastructure virtualbmc
 osism-kolla deploy ironic
