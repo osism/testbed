d9421371c74812b0628763c984de92547fd5572e
Christian Berendt <berendt@osism.tech>
2022-01-14 20:35:28 +0100

    netbox: connect & disable (#1010)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- terraform/files/manager.sh
+++ terraform/files/manager.sh
@@ -78,6 +78,8 @@ sudo -iu dragon sh -c 'INTERACTIVE=false osism netbox import --vendors Mikrotik'
 sudo -iu dragon sh -c 'INTERACTIVE=false osism netbox import --vendors Other --no-library'
 sudo -iu dragon sh -c 'INTERACTIVE=false osism netbox init'
 sudo -iu dragon sh -c 'INTERACTIVE=false osism netbox manage 1000'
+sudo -iu dragon sh -c 'INTERACTIVE=false osism netbox connect 1000'
+sudo -iu dragon sh -c 'INTERACTIVE=false osism netbox disable testbed-switch-0'
 
 sudo -iu dragon sh -c 'INTERACTIVE=false osism apply operator -l "all:!manager" -u ubuntu'
 sudo -iu dragon sh -c 'INTERACTIVE=false osism apply --environment custom facts'
