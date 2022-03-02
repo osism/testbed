e9dd6bf55d4627c9f29ba5ac1218d75d697aa71d
Christian Berendt <berendt@osism.tech>
2022-03-02 10:22:32 +0100

    Use ansible.netcommon.ipaddr
    Closes #1072
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- network/vxlan.sh
+++ network/vxlan.sh
@@ -2,6 +2,6 @@
 
 if [[ $IFACE == "{{ ansible_local.testbed_network_devices.management }}" ]]; then
     ip link add vxlan0 type vxlan id 42 group 239.1.1.1 dstport 4789 dev {{ ansible_local.testbed_network_devices.management }}
-    ip addr add {{ '192.168.112.0/20' | ipaddr('net') | ipaddr(node_id) | ipaddr('address') }}/20 dev vxlan0
+    ip addr add {{ '192.168.112.0/20' | ansible.netcommon.ipaddr('net') | ansible.netcommon.ipaddr(node_id) | ansible.netcommon.ipaddr('address') }}/20 dev vxlan0
     ip link set up dev vxlan0
 fi
