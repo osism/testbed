2f229c89cf797902e9e5317728783a77193b7fdb
Christian Berendt <berendt@osism.tech>
2023-01-29 15:53:52 +0100

    docs: use material theme (#1441)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,27 +19,24 @@ language = 'en'
 exclude_patterns = []
 pygments_style = 'sphinx'
 todo_include_todos = True
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_material'
 html_show_sphinx = False
 html_show_sourcelink = False
 html_show_copyright = True
 htmlhelp_basename = 'documentation'
 html_theme_options = {
-    'display_version': False,
-    'canonical_url': 'https://docs.osism.tech/testbed/',
-    'style_external_links': True,
-    'logo_only': True,
-    'prev_next_buttons_location': None
-}
-html_context = {
-    'display_github': True,
-    'github_user': 'osism',
-    'github_repo': 'testbed',
-    'github_version': 'main',
-    'conf_py_path': '/docs/source/'
+    "nav_title": "OSISM Documentation",
+    "color_primary": "blue",
+    "color_accent": "light-blue",
+    "globaltoc_depth": 3,
+    "globaltoc_collapse": True,
 }
 html_logo = 'images/logo.png'
 html_static_path = [
     '_static'
 ]
+html_title = "OSISM Documentation"
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
 latex_elements = {}
