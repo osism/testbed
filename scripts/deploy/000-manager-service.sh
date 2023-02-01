150f54aab4b8754f644b4bb7405c32c191c5548c
Christian Berendt <berendt@osism.tech>
2023-02-01 22:47:43 +0100

    Fix docker namespace for stable OSISM kolla images
    On harbor.services.osism.tech the images are located in
    the namespace kolla. On quay.io they are in the namespace
    osism.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/000-manager-service.sh
+++ scripts/deploy/000-manager-service.sh
@@ -21,6 +21,7 @@ else
     sed -i "s/docker_registry_ansible: .*/docker_registry_ansible: quay.io/g" /opt/configuration/environments/manager/configuration.yml
     sed -i "s/ceph_docker_registry: .*/ceph_docker_registry: quay.io/g" /opt/configuration/environments/ceph/configuration.yml
     sed -i "s/docker_registry_kolla: .*/docker_registry_kolla: quay.io/g" /opt/configuration/environments/kolla/configuration.yml
+    sed -i "s/docker_namespace: .*/docker_namespace: osism/g" /opt/configuration/environments/kolla/configuration.yml
 fi
 
 wait_for_container_healthy() {
