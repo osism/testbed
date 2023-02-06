40ef636691c331b72ce74ecb1df7c450a36fa3e6
Ralf Heiringhoff <frosty-geek@users.noreply.github.com>
2023-02-06 09:57:31 +0100

    removed unused tf vars ref#1468
    Signed-off-by: Ralf Heiringhoff <frosty-geek@users.noreply.github.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -32,11 +32,6 @@ variable "flavor_node" {
   default = "SCS-8V:32:50"
 }
 
-variable "flavor_node_alt" {
-  type    = string
-  default = "SCS-8V:32:50"
-}
-
 variable "flavor_manager" {
   type    = string
   default = "SCS-4V:8:50"
@@ -52,19 +47,9 @@ variable "volume_availability_zone" {
   default = "south-2"
 }
 
-variable "network_availability_zone" {
-  type    = string
-  default = "south-2"
-}
-
 variable "public" {
   type    = string
   default = "external"
-}
-
-variable "port_security_enabled" {
-  type    = bool
-  default = false
 }
 
 variable "deploy_monitoring" {
@@ -100,11 +85,6 @@ variable "number_of_nodes" {
 variable "number_of_volumes" {
   type    = number
   default = 3
-}
-
-variable "enable_dhcp" {
-  type    = bool
-  default = false
 }
 
 variable "enable_config_drive" {
