a39c7d4b6bc8de5a9faa205629576184a8136c3d
Christian Berendt <berendt@osism.tech>
2023-03-16 20:15:59 +0100

    Deploy prometheus before ceilometer
    Closes osism/testbed#1542
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/310-openstack-services-extended.sh
+++ scripts/deploy/310-openstack-services-extended.sh
@@ -3,7 +3,10 @@ set -e
 
 export INTERACTIVE=false
 
-osism apply gnocchi
+task_ids=$(osism apply --no-wait --format script gnocchi 2>&1)
+task_ids+=" "$(osism apply --no-wait --format script prometheus 2>&1)
+
+osism wait --output --format script --delay 2 $task_ids
 
 task_ids=$(osism apply --no-wait --format script ceilometer2>&1)
 task_ids+=" "$(osism apply --no-wait --format script heat 2>&1)
