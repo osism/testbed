a06f0fc80bd0721bd728ce09c33d0843f309d1d9
Dr. Jens Harbott <86402919+osfrickler@users.noreply.github.com>
2023-03-16 20:16:49 +0100

    Fix idempotency for deployment script (#1543)
    When a deployment was interrupted and restarted, there would be errors
    in the disable-ara.sh script. Simply ignore those errors.
    
    Signed-off-by: Dr. Jens Harbott <harbott@osism.tech>

--- scripts/disable-ara.sh
+++ scripts/disable-ara.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker exec -t ceph-ansible mv /ansible/ara.env /ansible/ara.env.disabled
-docker exec -t kolla-ansible mv /ansible/ara.env /ansible/ara.env.disabled
-docker exec -t osism-ansible mv /ansible/ara.env /ansible/ara.env.disabled
+docker exec -t ceph-ansible mv /ansible/ara.env /ansible/ara.env.disabled || true
+docker exec -t kolla-ansible mv /ansible/ara.env /ansible/ara.env.disabled || true
+docker exec -t osism-ansible mv /ansible/ara.env /ansible/ara.env.disabled || true
