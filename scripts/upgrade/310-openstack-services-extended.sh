f6fe04dfc85868477c5156e61eff37019dfff2c7
Christian Berendt <berendt@osism.tech>
2023-03-17 07:28:24 +0100

    Deploy prometheus before ceilometer (#1545)
    Closes osism/testbed#1542
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/upgrade/310-openstack-services-extended.sh
+++ scripts/upgrade/310-openstack-services-extended.sh
@@ -3,7 +3,10 @@ set -e
 
 export INTERACTIVE=false
 
-osism apply gnocchi -e kolla_action=upgrade
+task_ids=$(osism apply --no-wait --format script gnocchi -e kolla_action=upgrade 2>&1)
+task_ids+=" "$(osism apply --no-wait --format script prometheus -e kolla_action=upgrade 2>&1)
+
+osism wait --output --format script --delay 2 $task_ids
 
 task_ids=$(osism apply --no-wait --format script ceilometer-e kolla_action=upgrade 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script heat -e kolla_action=upgrade 2>&1)
