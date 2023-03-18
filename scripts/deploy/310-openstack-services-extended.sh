5982a5b130cabc0277e68f596ad81c8569c9b04d
Christian Berendt <berendt@osism.tech>
2023-03-18 11:54:35 +0100

    Deploy/upgrade skyline service
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/310-openstack-services-extended.sh
+++ scripts/deploy/310-openstack-services-extended.sh
@@ -11,5 +11,6 @@ osism wait --output --format script --delay 2 $task_ids
 task_ids=$(osism apply --no-wait --format script ceilometer2>&1)
 task_ids+=" "$(osism apply --no-wait --format script heat 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script senlin 2>&1)
+task_ids+=" "$(osism apply --no-wait --format script skyline 2>&1)
 
 osism wait --output --format script --delay 2 $task_ids
--- scripts/upgrade/310-openstack-services-extended.sh
+++ scripts/upgrade/310-openstack-services-extended.sh
@@ -11,5 +11,6 @@ osism wait --output --format script --delay 2 $task_ids
 task_ids=$(osism apply --no-wait --format script ceilometer-e kolla_action=upgrade 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script heat -e kolla_action=upgrade 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script senlin -e kolla_action=upgrade 2>&1)
+task_ids+=" "$(osism apply --no-wait --format script skyline -e kolla_action=upgrade 2>&1)
 
 osism wait --output --format script --delay 2 $task_ids
