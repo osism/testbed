5ef96e1905fe893d7e25852233108cfba6a8f45e
Christian Berendt <berendt@osism.tech>
2023-05-08 14:51:03 +0200

    Skip skyline deployment before 5.0.0
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/310-openstack-services-extended.sh
+++ scripts/deploy/310-openstack-services-extended.sh
@@ -11,6 +11,12 @@ osism wait --output --format script --delay 2 $task_ids
 task_ids=$(osism apply --no-wait --format script ceilometer2>&1)
 task_ids+=" "$(osism apply --no-wait --format script heat 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script senlin 2>&1)
-task_ids+=" "$(osism apply --no-wait --format script skyline 2>&1)
+
+MANAGER_VERSION=$(docker inspect --format '{{ index .Config.Labels "org.opencontainers.image.version"}}' osism-ansible)
+if [[ $MANAGER_VERSION == "4.0.0" || $MANAGER_VERSION == "4.1.0" || $MANAGER_VERSION == "4.2.0" || $MANAGER_VERSION == "4.3.0" ]]; then
+    echo "Skip Skyline deployment before OSISM < 5.0.0"
+else
+    task_ids+=" "$(osism apply --no-wait --format script skyline 2>&1)
+fi
 
 osism wait --output --format script --delay 2 $task_ids
