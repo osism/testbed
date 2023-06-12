d00e01b63fa4b9aea824cb2694add5a71f690c03
Christian Berendt <berendt@osism.tech>
2023-06-12 20:28:51 +0200

    Use opensearch role instead of ek role where already possible (#1638)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/200-infrastructure-services-basic.sh
+++ scripts/deploy/200-infrastructure-services-basic.sh
@@ -8,16 +8,26 @@ osism apply loadbalancer
 
 task_ids=$(osism apply --no-wait --format script keycloak 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script openstackclient 2>&1)
-task_ids+=" "$(osism apply --no-wait --format script elasticsearch 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script memcached 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script redis 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script mariadb 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script rabbitmq 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script openvswitch 2>&1)
 
+MANAGER_VERSION=$(docker inspect --format '{{ index .Config.Labels "org.opencontainers.image.version"}}' osism-ansible)
+OPENSTACK_VERSION=$(docker inspect --format '{{ index .Config.Labels "de.osism.release.openstack" }}' kolla-ansible)
+
+if [[ $MANAGER_VERSION =~ ^4\.[0-9]\.[0-9]$ || $OPENSTACK_VERSION == "yoga" ]]; then
+    task_ids+=" "$(osism apply --no-wait --format script elasticsearch 2>&1)
+    if [[ "$REFSTACK" == "false" ]]; then
+        task_ids+=" "$(osism apply --no-wait --format script kibana 2>&1)
+    fi
+else
+    task_ids+=" "$(osism apply --no-wait --format script opensearch 2>&1)
+fi
+
 if [[ "$REFSTACK" == "false" ]]; then
     task_ids+=" "$(osism apply --no-wait --format script homer 2>&1)
-    task_ids+=" "$(osism apply --no-wait --format script kibana 2>&1)
     task_ids+=" "$(osism apply --no-wait --format script phpmyadmin 2>&1)
 fi
 
--- scripts/upgrade/200-infrastructure-services-basic.sh
+++ scripts/upgrade/200-infrastructure-services-basic.sh
@@ -8,11 +8,10 @@ osism apply common -e kolla_action=upgrade
 osism apply loadbalancer -e kolla_action=upgrade
 
 task_ids=$(osism apply --no-wait --format script openstackclient 2>&1)
-task_ids+=" "$(osism apply --no-wait --format script elasticsearch -e kolla_action=upgrade 2>&1)
+task_ids+=" "$(osism apply --no-wait --format script opensearch -e kolla_action=upgrade 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script memcached -e kolla_action=upgrade 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script redis -e kolla_action=upgrade 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script mariadb -e kolla_action=upgrade 2>&1)
-task_ids+=" "$(osism apply --no-wait --format script kibana -e kolla_action=upgrade 2>&1)
 task_ids+=" "$(osism apply --no-wait --format script rabbitmq -e kolla_action=upgrade 2>&1)
 
 osism wait --output --format script --delay 2 $task_ids
