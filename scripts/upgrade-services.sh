bc7cb87ec405fed598a6f90aaf75b4326e4e1dd6
Christian Berendt <berendt@osism.tech>
2023-09-04 22:21:16 +0200

    Prepare for 6.0.0a (pt. 1) (#1725)
    Not many changes in Ceph. In order to see errors in the Kolla more quickly,
    the basic infra services are deployed before.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/upgrade-services.sh
+++ scripts/upgrade-services.sh
@@ -5,11 +5,11 @@ set -e
 # pull images
 sh -c '/opt/configuration/scripts/000-pull-images.sh'
 
-# upgrade ceph services
-sh -c '/opt/configuration/scripts/upgrade/100-ceph-services.sh'
-
 # upgrade infrastructure services
 sh -c '/opt/configuration/scripts/upgrade/200-infrastructure-services-basic.sh'
+
+# upgrade ceph services
+sh -c '/opt/configuration/scripts/upgrade/100-ceph-services.sh'
 
 # upgrade openstack services
 sh -c '/opt/configuration/scripts/upgrade/300-openstack-services-basic.sh'
