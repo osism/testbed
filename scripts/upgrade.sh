95f89b8b1c33d2a6237aea96b5c6b986b972da54
Christian Berendt <berendt@osism.tech>
2023-09-13 21:40:34 +0200

    Use openstack 2023.1 by default
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -8,7 +8,7 @@ echo
 
 export MANAGER_VERSION=${1:-latest}
 CEPH_VERSION=${2:-quincy}
-OPENSTACK_VERSION=${3:-zed}
+OPENSTACK_VERSION=${3:-2023.1}
 KOLLA_NAMESPACE=${4:-kolla}
 
 /opt/configuration/scripts/set-manager-version.sh $MANAGER_VERSION
