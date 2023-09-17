f45d8c4a423f6461d6f163284352ee4fd741263d
Christian Berendt <berendt@osism.tech>
2023-09-17 14:53:12 +0200

    zuul: use quay.io for jobs running on external clouds (#1758)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ scripts/set-docker-registry.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+DOCKER_REGISTRY=${1:-quay.io}
+
+sed -i "s#docker_registry_ansible: .*#docker_registry_ansible: ${DOCKER_REGISTRY}#g" /opt/configuration/environments/configuration.yml
+sed -i "s#docker_registry_kolla: .*#docker_registry_kolla: ${DOCKER_REGISTRY}#g" /opt/configuration/environments/configuration.yml
+sed -i "s#docker_registry_netbox: .*#docker_registry_netbox: ${DOCKER_REGISTRY}#g" /opt/configuration/environments/configuration.yml
+sed -i "s#ceph_docker_registry: .*#ceph_docker_registry: ${DOCKER_REGISTRY}#g" /opt/configuration/environments/configuration.yml
