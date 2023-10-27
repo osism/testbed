4204a510fabf932982da6b0277079cffe992647c
Christian Berendt <berendt@osism.tech>
2023-10-27 20:00:36 +0200

    Use playbook tasks to download required ironic images (#1817)
    Closes osism/testbed#1801
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/upgrade/320-openstack-services-baremetal.sh
+++ scripts/upgrade/320-openstack-services-baremetal.sh
@@ -4,9 +4,4 @@ set -e
 
 export INTERACTIVE=false
 
-OPENSTACK_VERSION=$(docker inspect --format '{{ index .Config.Labels "de.osism.release.openstack"}}' kolla-ansible)
-
-curl https://tarballs.opendev.org/openstack/ironic-python-agent/dib/files/ipa-centos8-stable-${OPENSTACK_VERSION}.kernel -o /opt/configuration/environments/kolla/files/overlays/ironic/ironic-agent.kernel
-curl https://tarballs.opendev.org/openstack/ironic-python-agent/dib/files/ipa-centos8-stable-${OPENSTACK_VERSION}.initramfs -o /opt/configuration/environments/kolla/files/overlays/ironic/ironic-agent.initramfs
-
-osism apply -a upgrade ironic
+osism apply -a upgrade ironic -e enable_ironic_agent_download_images=true
