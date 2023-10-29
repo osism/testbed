e8d1231a3d9e763a89a22cc21aae53bcf1519365
Christian Berendt <berendt@osism.tech>
2023-10-29 08:25:33 +0100

    ironic: use dummy files
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/320-openstack-services-baremetal.sh
+++ scripts/deploy/320-openstack-services-baremetal.sh
@@ -3,4 +3,4 @@ set -e
 
 export INTERACTIVE=false
 
-osism apply ironic -e enable_ironic_agent_download_images=true
+osism apply ironic
--- scripts/upgrade/320-openstack-services-baremetal.sh
+++ scripts/upgrade/320-openstack-services-baremetal.sh
@@ -4,4 +4,4 @@ set -e
 
 export INTERACTIVE=false
 
-osism apply -a upgrade ironic -e enable_ironic_agent_download_images=true
+osism apply -a upgrade ironic
