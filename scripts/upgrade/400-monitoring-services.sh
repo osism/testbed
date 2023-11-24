9d19a21dc9f1d0839f9e4af3d355502d549e88f8
Matej Feder <matej.feder@dnation.cloud>
2023-11-24 14:07:00 +0100

    Add thanos sidecar deployment and variables
    Signed-off-by: Matej Feder <matej.feder@dnation.cloud>

--- scripts/upgrade/400-monitoring-services.sh
+++ scripts/upgrade/400-monitoring-services.sh
@@ -5,3 +5,7 @@ source /opt/configuration/scripts/include.sh
 
 osism apply -a upgrade prometheus
 osism apply -a upgrade grafana
+
+if [[ $MANAGER_VERSION =~ ^7\.[0-9]\.[0-9]?$ || $MANAGER_VERSION == "latest" ]]; then
+    osism apply thanos_sidecar
+fi
