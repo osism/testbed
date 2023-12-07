993e16c8649a3e9b47fc62f7b7a95fc880bfd7d5
Marc Schöchlin <schoechlin@osb-alliance.com>
2023-12-07 15:53:04 +0100

    weaken check
    Signed-off-by: Marc Schöchlin <schoechlin@osb-alliance.com>

--- contrib/setup-testbed.py
+++ contrib/setup-testbed.py
@@ -5,6 +5,7 @@ import subprocess
 import sys
 import yaml
 import os
+import time
 
 
 def lookup(yaml_path: str, local_data: dict) -> None | str | dict:
@@ -41,10 +42,18 @@ def check_environment(name: str) -> None:
 
     try:
         _check_file("terraform/clouds.yaml", name)
-        _check_file("terraform/secure.yaml", name)
     except Exception as e:
         print(e)
         sys.exit(1)
+
+    # TODO: as discussed in https://github.com/osism/testbed/pull/1879 we add a app credential check later
+    # to make a smart distinction if that check is needed or not.
+    try:
+        _check_file("terraform/secure.yaml", name)
+    except Exception as e:
+        print(e)
+        time.sleep(2)
+
     sys.exit(0)
 
 
