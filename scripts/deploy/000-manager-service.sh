89336d5326135bf6a72d6c464cb79b955a4bb3db
Christian Berendt <berendt@osism.tech>
2024-01-06 15:59:44 +0100

    Deploy & integrate nexus as passthrough cache for container images
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/000-manager-service.sh
+++ scripts/deploy/000-manager-service.sh
@@ -64,12 +64,12 @@ docker compose --project-directory /opt/manager ps
 docker compose --project-directory /opt/netbox ps
 
 # disable ara service
-if [[ -e /etc/osism-ci-image  ]]; then
+if [[ -e /etc/osism-ci-image ]]; then
     sh -c '/opt/configuration/scripts/disable-ara.sh'
 fi
 
 # use osism.commons.still_alive stdout callback
-if [[ "$MANAGER_VERSION" == "latest" ]];then
+if [[ "$MANAGER_VERSION" == "latest" ]]; then
     # The plugin is available in OSISM 6.1.0 and higher. In future, the callback
     # plugin will be used by default.
     sed -i "s/community.general.yaml/osism.commons.still_alive/" /opt/configuration/environments/ansible.cfg
@@ -77,6 +77,18 @@ fi
 
 osism apply sshconfig
 osism apply known-hosts
+
+if [[ "$MANAGER_VERSION" == "latest" ]]; then
+    # The Nexus service is only really operational again from OSISM 6.1.0.
+    osism apply nexus
+
+    if [[ -e /etc/osism-ci-image ]]; then
+        sh -c '/opt/configuration/scripts/set-docker-registry nexus.testbed.osism.xyz:8193'
+	sed -i "s/docker_namespace: osism/docker_namespace: kolla/" /opt/configuration/environments/kolla/configuration.yml
+    else
+        sh -c '/opt/configuration/scripts/set-docker-registry nexus.testbed.osism.xyz:8192'
+    fi
+fi
 
 osism apply squid
 
