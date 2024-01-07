9921846f5cb6c6b7cc15ada3b959cb9d1620709d
Christian Berendt <berendt@osism.tech>
2024-01-07 12:17:42 +0100

    Install helm & deploy kubernetes dashboard
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/005-kubernetes.sh
+++ scripts/deploy/005-kubernetes.sh
@@ -7,7 +7,7 @@ export INTERACTIVE=false
 
 osism apply k3s
 
-# NOTE: The following lines will be moved to an osism.services.clusterapi role
+# NOTE: The following lines will be moved to a osism.services.clusterapi role
 
 CAPI_VERSION="v1.5.1"
 CAPO_VERSION="v0.8.0"
@@ -35,3 +35,20 @@ clusterctl init \
   --bootstrap kubeadm:${CAPI_VERSION} \
    --control-plane kubeadm:${CAPI_VERSION} \
   --infrastructure openstack:${CAPO_VERSION}
+
+# NOTE: The following lines will be moved to a osism.commons.helm role
+
+# install helm
+
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+# NOTE: The following lines will be moved to a osism.services.kubernetes_dashboard role
+
+# deploy kubernetes dashboard
+
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
