fe799cc41d0e39be3b8ecb345a76755947ba1057
Christian Berendt <berendt@osism.tech>
2024-01-16 09:50:07 +0100

    Run openstack play in check/300-openstack-services.sh
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/bootstrap/300-openstack-services.sh
+++ scripts/bootstrap/300-openstack-services.sh
@@ -4,10 +4,9 @@ set -e
 source /opt/configuration/scripts/include.sh
 
 MANAGER_VERSION=$(docker inspect --format '{{ index .Config.Labels "org.opencontainers.image.version"}}' osism-ansible)
-OPENSTACK_VERSION=$(docker inspect --format '{{ index .Config.Labels "de.osism.release.openstack"}}' kolla-ansible)
 
 osism apply --environment openstack bootstrap-flavors
-osism apply --environment openstack bootstrap-basic -e openstack_version=$OPENSTACK_VERSION
+osism apply --environment openstack bootstrap-basic
 
 # osism manage images is only available since 5.0.0. To enable the
 # testbed to be used with < 5.0.0, here is this check.
@@ -15,8 +14,4 @@ if [[ $MANAGER_VERSION =~ ^4\.[0-9]\.[0-9]$ ]]; then
     osism apply --environment openstack bootstrap-images
 else
     osism manage images --cloud admin --filter Cirros
-fi
-
-if [[ "$REFSTACK" == "false" ]]; then
-    osism apply --environment openstack test
 fi
