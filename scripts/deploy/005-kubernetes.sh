5d734e57a09b6e90f569337baa61c9f68a25bb27
Christian Berendt <berendt@osism.tech>
2024-02-17 12:06:12 +0100

    Cleanup the 005-kubernetes deploy script
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/005-kubernetes.sh
+++ scripts/deploy/005-kubernetes.sh
@@ -6,38 +6,9 @@ source /opt/configuration/scripts/include.sh
 source /opt/manager-vars.sh
 
 osism apply kubernetes
-
-# NOTE: The following lines will be moved to a osism.services.clusterapi role
-
-CAPI_VERSION="v1.6.1"
-CAPO_VERSION="v0.9.0"
-
-export KUBECONFIG=$HOME/.kube/config
-
-# add openstack-control-plane label to all hosts labeled control-plane
-OS_CONTROL_PLANE_NODES=$(kubectl get nodes | grep control-plane | awk '{print $1}')
-for NODE in $OS_CONTROL_PLANE_NODES; do
-    kubectl label node "${NODE}" openstack-control-plane=enabled
-done
-
-# add worker node-role label to all hosts without a role
-NODES_WITHOUT_ROLE=$(kubectl get nodes | grep '<none>' | awk '{print $1}')
-for NODE in $NODES_WITHOUT_ROLE; do
-    kubectl label node "${NODE}" node-role.kubernetes.io/worker=worker
-done
-
-sudo curl -Lo /usr/local/bin/clusterctl https://github.com/kubernetes-sigs/cluster-api/releases/download/${CAPI_VERSION}/clusterctl-linux-amd64
-sudo chmod +x /usr/local/bin/clusterctl
-export EXP_CLUSTER_RESOURCE_SET=true
-export CLUSTER_TOPOLOGY=true
-clusterctl init \
-  --core cluster-api:${CAPI_VERSION} \
-  --bootstrap kubeadm:${CAPI_VERSION} \
-   --control-plane kubeadm:${CAPI_VERSION} \
-  --infrastructure openstack:${CAPO_VERSION}
+osism apply clusterapi
 
 # NOTE: The following lines will be moved to a osism.commons.helm role
-
 # install helm
 
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
@@ -46,8 +17,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.
 sudo apt-get update
 sudo apt-get install helm
 
-# NOTE: The following lines will be moved to a osism.services.kubernetes_dashboard role
-
+# NOTE: The following lines will be moved to a kubernetes_dashboard play
 # deploy kubernetes dashboard
 
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
