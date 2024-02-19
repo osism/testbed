489e378876eed4e501f4f4e79a9caa2ce9449f73
Christian Berendt <berendt@osism.tech>
2024-02-19 20:52:49 +0100

    Adapt the upgrade of Octavia for OSISM >= 7.0.0
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/upgrade/300-openstack-services-basic.sh
+++ scripts/upgrade/300-openstack-services-basic.sh
@@ -13,6 +13,15 @@ osism apply -a upgrade neutron
 osism apply -a upgrade cinder
 osism apply -a upgrade barbican
 osism apply -a upgrade designate
+
+# In OSISM >= 7.0.0 the persistence feature in Octavia was enabled by default.
+# This requires an additional database, which is only created when Octavia play
+# is run in bootstrap mode first.
+if [[ $MANAGER_VERSION =~ ^7\.[0-9]\.[0-9][a-z]?$ || $MANAGER_VERSION == "latest" ]]; then
+    # NOTE: can be replaced by osism apply -a bootstrap octavia after the release of OSISM 7.0.0b
+    osism apply octavia -e kolla_action=bootstrap
+fi
+
 osism apply -a upgrade octavia
 
 if [[ $MANAGER_VERSION =~ ^7\.[0-9]\.[0-9][a-z]?$ || $MANAGER_VERSION == "latest" ]]; then
