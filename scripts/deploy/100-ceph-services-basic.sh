3c7202ef6e69fdeea4a6fc73a59a8d36e53c20ab
Christian Berendt <berendt@osism.tech>
2024-02-28 21:41:15 +0100

    ceph: run reconciler & remove devices from inventory (#2074)
    Related to c1acecea1ce5e6703f997137703add1abb5f5baf
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/100-ceph-services-basic.sh
+++ scripts/deploy/100-ceph-services-basic.sh
@@ -22,13 +22,14 @@ fi
 # In preparation for deployment with Rook, the pre-built LVM2 volumes are always used
 # from OSISM 7 onwards.
 if [[ $MANAGER_VERSION =~ ^7\.[0-9]\.[0-9]$ || $MANAGER_VERSION == "latest" ]]; then
+    sed -i "/^devices:/d" /opt/configuration/inventory/group_vars/testbed-nodes.yml
     osism apply ceph-configure-lvm-volumes
     for node in $(find /opt/configuration/inventory/host_vars -mindepth 1 -type d); do
         if [[ -e /tmp/$(basename $node)-ceph-lvm-configuration.yml ]]; then
             cp /tmp/$(basename $node)-ceph-lvm-configuration.yml /opt/configuration/inventory/host_vars/$(basename $node)/ceph-lvm-configuration.yml
         fi
     done
-    osism apply facts
+    osism reconciler sync
     osism apply ceph-create-lvm-devices
 fi
 
