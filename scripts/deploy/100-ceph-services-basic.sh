c1acecea1ce5e6703f997137703add1abb5f5baf
Christian Berendt <berendt@osism.tech>
2024-02-28 21:06:17 +0100

    ceph: use pre-built LVM2 volumes (#2073)
    Related to osism/issues#874
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/100-ceph-services-basic.sh
+++ scripts/deploy/100-ceph-services-basic.sh
@@ -19,6 +19,19 @@ if [[ $MANAGER_VERSION == "latest" && $CEPH_VERSION == "pacific" ]]; then
     sed -i "s/osism.commons.still_alive/community.general.yaml/" /opt/configuration/environments/ansible.cfg
 fi
 
+# In preparation for deployment with Rook, the pre-built LVM2 volumes are always used
+# from OSISM 7 onwards.
+if [[ $MANAGER_VERSION =~ ^7\.[0-9]\.[0-9]$ || $MANAGER_VERSION == "latest" ]]; then
+    osism apply ceph-configure-lvm-volumes
+    for node in $(find /opt/configuration/inventory/host_vars -mindepth 1 -type d); do
+        if [[ -e /tmp/$(basename $node)-ceph-lvm-configuration.yml ]]; then
+            cp /tmp/$(basename $node)-ceph-lvm-configuration.yml /opt/configuration/inventory/host_vars/$(basename $node)/ceph-lvm-configuration.yml
+        fi
+    done
+    osism apply facts
+    osism apply ceph-create-lvm-devices
+fi
+
 if [[ $MANAGER_VERSION =~ ^4\.[0-9]\.[0-9]$ ]]; then
     osism apply ceph-base
     osism apply ceph-mdss
