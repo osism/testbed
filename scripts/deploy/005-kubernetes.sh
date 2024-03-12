f31a7937131ece2dc1c209ccf2370b16cd3bd07e
Christian Berendt <berendt@osism.tech>
2024-03-12 20:45:58 +0100

    Use kubernetes-dashboard play (#2096)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/005-kubernetes.sh
+++ scripts/deploy/005-kubernetes.sh
@@ -8,17 +8,6 @@ source /opt/manager-vars.sh
 osism apply kubernetes
 osism apply clusterapi
 
-# NOTE: The following lines will be moved to a osism.commons.helm role
-# install helm
-
-curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-sudo apt-get install apt-transport-https --yes
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install helm
-
-# NOTE: The following lines will be moved to a kubernetes_dashboard play
-# deploy kubernetes dashboard
-
-helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+if [[ $MANAGER_VERSION =~ ^7\.[0-9]\.[0-9]$ || $MANAGER_VERSION == "latest" ]]; then
+    osism apply kubernetes-dashboard
+fi
