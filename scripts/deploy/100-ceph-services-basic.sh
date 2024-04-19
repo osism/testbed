cbe1c6cfcfdf741eab1729e837b32ef65b932eb8
Christian Berendt <berendt@osism.tech>
2024-04-19 20:15:18 +0200

    Add a separate script to prepare the ceph configuration & devices (#2156)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/100-ceph-services-basic.sh
+++ scripts/deploy/100-ceph-services-basic.sh
@@ -3,33 +3,16 @@ set -e
 
 source /opt/configuration/scripts/include.sh
 
+sh -c '/opt/configuration/scripts/prepare-ceph-configuration.sh'
+
 MANAGER_VERSION=$(docker inspect --format '{{ index .Config.Labels "org.opencontainers.image.version"}}' osism-ansible)
 CEPH_VERSION=$(docker inspect --format '{{ index .Config.Labels "de.osism.release.ceph" }}' ceph-ansible)
-
-# Make sure that no partitions are present
-osism apply --environment custom wipe-partitions
-osism apply facts
 
 # The callback plugin is not included in the Pacific image. The plugin is no longer
 # added there because the builds for Pacific are disabled. This callback plugin will
 # therefore not be used during the deployment of Ceph.
 if [[ $MANAGER_VERSION == "latest" && $CEPH_VERSION == "pacific" ]]; then
     sed -i "s/osism.commons.still_alive/community.general.yaml/" /opt/configuration/environments/ansible.cfg
-fi
-
-# In preparation for deployment with Rook, the pre-built LVM2 volumes are always used
-# from OSISM 7 onwards.
-if [[ $MANAGER_VERSION =~ ^7\.[0-9]\.[0-9]$ || $MANAGER_VERSION == "latest" ]]; then
-    sed -i "/^devices:/d" /opt/configuration/inventory/group_vars/testbed-nodes.yml
-    osism apply ceph-configure-lvm-volumes
-    for node in $(find /opt/configuration/inventory/host_vars -mindepth 1 -type d); do
-        if [[ -e /tmp/$(basename $node)-ceph-lvm-configuration.yml ]]; then
-            cp /tmp/$(basename $node)-ceph-lvm-configuration.yml /opt/configuration/inventory/host_vars/$(basename $node)/ceph-lvm-configuration.yml
-        fi
-    done
-    osism reconciler sync
-    osism apply ceph-create-lvm-devices
-    osism apply facts
 fi
 
 if [[ $MANAGER_VERSION =~ ^4\.[0-9]\.[0-9]$ ]]; then
