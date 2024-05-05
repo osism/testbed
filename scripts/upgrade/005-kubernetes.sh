31ab2112e525e005b6950739ceaad25793f12e43
Christian Berendt <berendt@osism.tech>
2024-05-05 11:43:39 +0200

    Fix upgrade scripts (#2181)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/upgrade/005-kubernetes.sh
+++ scripts/upgrade/005-kubernetes.sh
@@ -4,5 +4,12 @@ set -e
 
 source /opt/configuration/scripts/include.sh
 
-osism apply kubernetes
-osism apply clusterapi
+MANAGER_VERSION=$(docker inspect --format '{{ index .Config.Labels "org.opencontainers.image.version"}}' osism-ansible)
+
+if [[ $MANAGER_VERSION == "latest" ]]; then
+    osism apply kubernetes
+fi
+
+if [[ $MANAGER_VERSION =~ ^7\.[0-9]\.[0-9]?$ || $MANAGER_VERSION == "latest" ]]; then
+    osism apply clusterapi
+fi
