3d0de5bbee79fdea2b09bcee19f9ce767536a2ed
Christian Berendt <berendt@osism.tech>
2024-05-21 20:34:39 +0200

    Do not use keycloak/keystone federation with external api (pt. 2)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/customisations/external-api.sh
+++ scripts/customisations/external-api.sh
@@ -36,8 +36,11 @@ sed -i "s/auth_url: .*/auth_url: https:\/\/${api_fqdn}:5000\/v3/g" /opt/configur
 # overwrite fqdn for internal use
 sed -i "s/api.testbed.osism.xyz: .*/${api_fqdn}: 192.168.16.254/g" /opt/configuration/environments/configuration.yml
 
-# do not use keystone logout url
+# remove keystone/keycloak federation integration
 sed -i "/keystone_logout_url: .*/d" /opt/configuration/environments/kolla/configuration.yml
+rm /opt/configuration/environments/kolla/files/overlays/horizon/custom_local_settings
+rm /opt/configuration/environments/kolla/files/overlays/keystone/wsgi-keystone.conf
+rm -rf /opt/configuration/environments/kolla/files/overlays/keystone/federation
 
 # add traefik ports + services + routers
 cat >> /opt/configuration/environments/infrastructure/configuration.yml <<%EOF
