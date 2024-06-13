43f2a129c7b5160f22c37940403f366771b1a519
Christian Berendt <berendt@osism.tech>
2024-06-13 19:01:22 +0200

    Remove some more files to disable Keystone/Keycloak integration
    Related to 67cd6791c4122075bc94506df9adacf482c85717
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/upgrade/300-openstack-services-basic.sh
+++ scripts/upgrade/300-openstack-services-basic.sh
@@ -9,6 +9,8 @@ MANAGER_VERSION=$(docker inspect --format '{{ index .Config.Labels "org.opencont
 # Do not use the Keystone/Keycloak integration by default. We only use this integration
 # in a special identity testbed.
 rm -f /opt/configuration/environments/kolla/group_vars/keystone.yml
+rm -f /opt/configuration/environments/kolla/files/overlays/keystone/wsgi-keystone.conf
+rm -rf /opt/configuration/environments/kolla/files/overlays/keystone/federation
 
 osism apply -a upgrade keystone
 osism apply -a upgrade placement
