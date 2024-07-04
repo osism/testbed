068f984b8db4d645839f19989da1bf303a5f3dd7
Christian Berendt <berendt@osism.tech>
2024-07-04 10:55:11 +0200

    Only disable ARA in ceph-ansible container if ceph-ansible is in use
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/disable-ara.sh
+++ scripts/disable-ara.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-docker exec -t ceph-ansible mv /ansible/ara.env /ansible/ara.env.disabled || true
+if [[ $CEPH_STACK == "ceph-ansible" ]]; then
+    docker exec -t ceph-ansible mv /ansible/ara.env /ansible/ara.env.disabled || true
+fi
 docker exec -t kolla-ansible mv /ansible/ara.env /ansible/ara.env.disabled || true
 docker exec -t osism-ansible mv /ansible/ara.env /ansible/ara.env.disabled || true
