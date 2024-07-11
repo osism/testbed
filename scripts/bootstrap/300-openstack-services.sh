94b20c354b9f0dba049060e44264aaa7d2228d07
Christian Berendt <berendt@osism.tech>
2024-07-11 15:50:52 +0200

    Fix wrong manager version check (#2294)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/bootstrap/300-openstack-services.sh
+++ scripts/bootstrap/300-openstack-services.sh
@@ -5,7 +5,7 @@ source /opt/configuration/scripts/include.sh
 
 MANAGER_VERSION=$(docker inspect --format '{{ index .Config.Labels "org.opencontainers.image.version"}}' osism-ansible)
 
-if [[ $MANAGER_VERSION =~ ^7\.[0-9]\.[2-9]$ || $MANAGER_VERSION == "latest" ]]; then
+if [[ $MANAGER_VERSION =~ ^7\.0\.[2-9]$ || $MANAGER_VERSION =~ ^7\.[1-9]\.[0-9]$ || $MANAGER_VERSION == "latest" ]]; then
     osism manage flavors --recommended
 else
     osism apply --environment openstack bootstrap-flavors
