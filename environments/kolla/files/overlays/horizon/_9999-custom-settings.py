8f68834d8026c753534b7d268a8975f234ed1786
Christian Berendt <berendt@osism.tech>
2024-07-22 09:37:11 +0200

    Add _9999-custom-settings.py file (#2322)
    Required by OpenStack Kolla >= 2024.1
    
    Related to osism/issues#1073
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- environments/kolla/files/overlays/horizon/_9999-custom-settings.py
+++ environments/kolla/files/overlays/horizon/_9999-custom-settings.py
@@ -1,4 +1,4 @@
-# This file is required up to OpenStack Kolla 2023.2.
+# This file is required as of OpenStack Kolla 2024.1.
 
 LOGOUT_URL = "https://keycloak.testbed.osism.xyz/auth/realms/osism/protocol/openid-connect/logout/?client_id=keystone&post_logout_redirect_uri=https%3A%2F%2Fapi.testbed.osism.xyz%3A5000%2Fredirect_uri%3Flogout%3Dhttps%3A%2F%2Fapi.testbed.osism.xyz%3A5000%2Flogout"
 
