75d586f70dddd5e426deb2fc8549eb6671d08012
Christian Berendt <berendt@osism.tech>
2024-08-12 19:57:42 +0200

    Only upload the 1.30 capi image
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/bootstrap/302-openstack-k8s-clusterapi-images.sh
+++ scripts/bootstrap/302-openstack-k8s-clusterapi-images.sh
@@ -6,5 +6,5 @@ source /opt/configuration/scripts/include.sh
 MANAGER_VERSION=$(docker inspect --format '{{ index .Config.Labels "org.opencontainers.image.version"}}' osism-ansible)
 
 if [[ $MANAGER_VERSION =~ ^7\.[0-9]\.[0-9]$ || $MANAGER_VERSION == "latest" ]]; then
-    osism manage image clusterapi
+    osism manage image clusterapi --filter 1.30
 fi
