8200aa08184bf773fbee3c04b501de021ace4eb7
Christian Berendt <berendt@osism.tech>
2024-08-22 10:01:31 +0200

    Add script to disable the use of the local registry
    Part of osism/testbed#2357
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy-manager.sh
+++ scripts/deploy-manager.sh
@@ -92,6 +92,7 @@ sudo ln -sf /opt/configuration/scripts/bootstrap/302-openstack-k8s-clusterapi-im
 
 # create symlinks for other scripts
 sudo ln -sf /opt/configuration/scripts/pull-images.sh /usr/local/bin/pull-images
+sudo ln -sf /opt/configuration/scripts/disable-local-registry.sh /usr/local/bin/disable-local-registry
 
 if [[ "$EXTERNAL_API" == "true" ]]; then
     sh -c '/opt/configuration/scripts/customisations/external-api.sh'
