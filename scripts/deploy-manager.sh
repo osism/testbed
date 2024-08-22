beafa57fe691f78211fb80f04780a42819f6ee69
Christian Berendt <berendt@osism.tech>
2024-08-22 20:52:15 +0200

    zuul: add multi distribution support
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy-manager.sh
+++ scripts/deploy-manager.sh
@@ -38,13 +38,7 @@ osism apply network
 osism apply wireguard
 
 # prepare wireguard configuration
-if [[ -e /home/dragon/wg0-dragon.conf ]]; then
-    # on OSISM < 5.0.0 this file is not yet present.
-    mv /home/dragon/wg0-dragon.conf /home/dragon/wireguard-client.conf
-fi
-
-sed -i -e s/WIREGUARD_PUBLIC_IP_ADDRESS/$(curl my.ip.fi)/ /home/dragon/wireguard-client.conf
-sed -i -e "s/CHANGEME - dragon private key/GEQ5eWshKW+4ZhXMcWkAAbqzj7QA9G64oBFB3CbrR0w=/" /home/dragon/wireguard-client.conf
+sh -c '/opt/configuration/script/prepare-wireguard-configuration.sh'
 
 # apply workarounds
 osism apply --environment custom workarounds
