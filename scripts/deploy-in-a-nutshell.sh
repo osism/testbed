29e3739df0ac20245cf30f04f079a0ec5e125d7b
Christian Berendt <berendt@osism.tech>
2024-08-23 09:58:11 +0200

    zuul: add multi distribution support (#2404)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy-in-a-nutshell.sh
+++ scripts/deploy-in-a-nutshell.sh
@@ -7,20 +7,7 @@ source /opt/configuration/scripts/include.sh
 sh -c '/opt/configuration/scripts/pull-images.sh'
 
 # prepare the ceph deployment
-
-osism apply --environment custom wipe-partitions
-osism apply facts
-
-sed -i "/^devices:/d" /opt/configuration/inventory/group_vars/testbed-nodes.yml
-osism apply ceph-configure-lvm-volumes
-for node in $(find /opt/configuration/inventory/host_vars -mindepth 1 -type d); do
-    if [[ -e /tmp/$(basename $node)-ceph-lvm-configuration.yml ]]; then
-        cp /tmp/$(basename $node)-ceph-lvm-configuration.yml /opt/configuration/inventory/host_vars/$(basename $node)/ceph-lvm-configuration.yml
-    fi
-done
-osism reconciler sync
-osism apply ceph-create-lvm-devices
-osism apply facts
+sh -c '/opt/configuration/scripts/prepare-ceph-configuration.sh'
 
 # deploy everything
 
@@ -31,7 +18,6 @@ echo
 osism apply nutshell
 
 # wait for all deployments
-
 osism wait --output --refresh 20
 
 echo
