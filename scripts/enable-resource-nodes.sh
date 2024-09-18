8b536e3a837d7e0a011071ea77837f13687497c5
Christian Berendt <berendt@osism.tech>
2024-09-18 20:39:16 +0200

    Also enable testbed-node-3 (#2469)
    Related to 3a94d651776fb6ba7b85767b489116265101bbd3
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/enable-resource-nodes.sh
+++ scripts/enable-resource-nodes.sh
@@ -5,7 +5,7 @@ source /opt/manager-vars.sh
 number_of_resource_nodes=$(expr $NUMBER_OF_NODES - 3)
 
 if [[ $number_of_resource_nodes -gt 0 ]]; then
-    for node in $(seq 4 $(expr $NUMBER_OF_NODES - 1)); do
+    for node in $(seq 3 $(expr $NUMBER_OF_NODES - 1)); do
       sed -i "/^#.*testbed-node-$node/s/^#//" /opt/configuration/inventory/10-custom
     done
 fi
