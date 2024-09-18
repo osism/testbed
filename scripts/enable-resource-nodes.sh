4f1e7dc1456f5aeec219d13ba802a0926201cab9
Christian Berendt <berendt@osism.tech>
2024-09-18 19:35:20 +0200

    Enable available resource nodes
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ scripts/enable-resource-nodes.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source /opt/manager-vars.sh
+
+number_of_resource_nodes=$(expr $NUMBER_OF_NODES - 3)
+
+if [[ $number_of_resource_nodes -gt 0 ]]; then
+    for node in $(seq 4 $(expr $NUMBER_OF_NODES - 1)); do
+      sed -i "/^#.*testbed-node-$node/s/^#//" /opt/configuration/inventory/10-custom
+    done
+fi
--- scripts/enable-secondary-nodes.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-number_of_secondary_nodes=$(expr $1 - 3)
-
-if [[ $number_of_secondary_nodes -gt 0 ]]; then
-    for node in $(seq 4 $(expr $1 - 1)); do
-      sed -i "/^#.*testbed-node-$node/s/^#//" /opt/configuration/inventory/10-custom
-    done
-fi
