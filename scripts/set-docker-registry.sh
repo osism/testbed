2a99acf7958c9a87acad4576a8865b5e467b3e9d
Christian Berendt <berendt@osism.tech>
2024-09-19 14:12:44 +0200

    Use the local registry in the nutshell deployment (#2473)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/set-docker-registry.sh
+++ scripts/set-docker-registry.sh
@@ -2,6 +2,8 @@
 set -x
 set -e
 
+source /opt/manager-vars.sh
+
 DOCKER_REGISTRY=${1:-osism.harbor.regio.digital}
 
 sed -i "s#ceph_docker_registry: .*#ceph_docker_registry: ${DOCKER_REGISTRY}#g" /opt/configuration/environments/configuration.yml
@@ -10,5 +12,9 @@ sed -i "s#docker_registry_kolla: .*#docker_registry_kolla: ${DOCKER_REGISTRY}#g"
 sed -i "s#docker_registry_netbox: .*#docker_registry_netbox: ${DOCKER_REGISTRY}#g" /opt/configuration/environments/configuration.yml
 
 if [[ "$DOCKER_REGISTRY" == "osism.harbor.regio.digital" ]]; then
-    sed -i "s/docker_namespace: osism/docker_namespace: kolla/" /opt/configuration/environments/kolla/configuration.yml
+    if [[ "$MANAGER_VERSION" == "latest" ]]; then
+        sed -i "s#docker_namespace: osism#docker_namespace: kolla#" /opt/configuration/environments/kolla/configuration.yml
+    else
+        sed -i "s#docker_namespace: osism#docker_namespace: kolla/release#" /opt/configuration/environments/kolla/configuration.yml
+    fi
 fi
