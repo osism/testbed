e851df0672f55fd11f6289f61ba669a8e2d5fca2
Christian Berendt <berendt@osism.tech>
2024-09-19 14:02:28 +0200

    Use the local registry in the nutshell deployment
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy-in-a-nutshell.sh
+++ scripts/deploy-in-a-nutshell.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# do not use the local registry
-sh -c '/opt/configuration/scripts/set-docker-registry.sh'
-
 # pull images
 sh -c '/opt/configuration/scripts/pull-images.sh'
 
