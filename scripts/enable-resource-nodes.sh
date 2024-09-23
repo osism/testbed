e45c7f4a34233e7fbc569aa6f21817a1abece294
Christian Berendt <berendt@osism.tech>
2024-09-23 16:40:17 +0200

    Use testbed-node-[0-2] on small testbed as resource nodes
    Related to 3a94d651776fb6ba7b85767b489116265101bbd3
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/enable-resource-nodes.sh
+++ scripts/enable-resource-nodes.sh
@@ -8,4 +8,8 @@ if [[ $number_of_resource_nodes -gt 0 ]]; then
     for node in $(seq 3 $(expr $NUMBER_OF_NODES - 1)); do
       sed -i "/^#.*testbed-node-$node/s/^#//" /opt/configuration/inventory/10-custom
     done
+else
+    for node in $(seq 0 $(expr $NUMBER_OF_NODES - 1)); do
+      sed -i "/^#.*testbed-node-$node/s/^#//" /opt/configuration/inventory/10-custom
+    done
 fi
