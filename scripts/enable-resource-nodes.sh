9a65b0d89557081479d062be34c853e9ac540516
Christian Berendt <berendt@osism.tech>
2024-09-23 22:21:51 +0200

    Do not use testbed-node-[0-2] as k3s nodes in a small testbed
    Related to 3a94d651776fb6ba7b85767b489116265101bbd3
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/enable-resource-nodes.sh
+++ scripts/enable-resource-nodes.sh
@@ -5,11 +5,12 @@ source /opt/manager-vars.sh
 number_of_resource_nodes=$(expr $NUMBER_OF_NODES - 3)
 
 if [[ $number_of_resource_nodes -gt 0 ]]; then
+    sed -i "/^#testbed-resource-nodes/s/^#//" /opt/configuration/inventory/20-roles
     for node in $(seq 3 $(expr $NUMBER_OF_NODES - 1)); do
-      sed -i "/^#.*testbed-node-$node/s/^#//" /opt/configuration/inventory/10-custom
+      sed -i "/^#testbed-node-$node/s/^#//" /opt/configuration/inventory/10-custom
     done
 else
     for node in $(seq 0 $(expr $NUMBER_OF_NODES - 1)); do
-      sed -i "/^#.*testbed-node-$node/s/^#//" /opt/configuration/inventory/10-custom
+      sed -i "/^#testbed-node-$node/s/^#//" /opt/configuration/inventory/10-custom
     done
 fi
