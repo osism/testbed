246857a029bddab36793c0de6ba0735026cc1755
Christian Berendt <berendt@osism.tech>
2024-09-25 21:13:45 +0200

    k3s: use cilium with BGP
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/500-kubernetes.sh
+++ scripts/deploy/500-kubernetes.sh
@@ -5,7 +5,7 @@ set -e
 source /opt/configuration/scripts/include.sh
 source /opt/manager-vars.sh
 
-if [[ $(semver $MANAGER_VERSION 8.0.0) -ge 0 || $MANAGER_VERSION == "latest" ]]; then
+if [[ $(semver $MANAGER_VERSION 8.0.3) -ge 0 || $MANAGER_VERSION == "latest" ]]; then
     osism apply kubernetes
     osism apply copy-kubeconfig
     osism apply k8s-dashboard
