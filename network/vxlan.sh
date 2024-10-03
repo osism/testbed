c60d28a2a2376194684b8d77e5211f9d445bd78f
Christian Berendt <berendt@osism.tech>
2024-10-03 19:19:15 +0200

    Use internal_interface instead of testbed_network_devices.management (#2504)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- network/vxlan.sh
+++ network/vxlan.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ $IFACE == "{{ ansible_local.testbed_network_devices.management }}" ]]; then
-    ip link add vxlan0 type vxlan id 42 group 239.1.1.1 dstport 4789 dev {{ ansible_local.testbed_network_devices.management }}
+if [[ $IFACE == "{{ internal_interface }}" ]]; then
+    ip link add vxlan0 type vxlan id 42 group 239.1.1.1 dstport 4789 dev {{ internal_interface }}
     ip addr add {{ '192.168.112.0/20' | ansible.utils.ipaddr('net') | ansible.utils.ipaddr(node_id) | ansible.utils.ipaddr('address') }}/20 dev vxlan0
     ip link set up dev vxlan0
 
-    ip link add vxlan1 type vxlan id 23 group 239.1.1.1 dstport 4789 dev {{ ansible_local.testbed_network_devices.management }}
+    ip link add vxlan1 type vxlan id 23 group 239.1.1.1 dstport 4789 dev {{ internal_interface }}
     ip addr add {{ '192.168.128.0/20' | ansible.utils.ipaddr('net') | ansible.utils.ipaddr(node_id) | ansible.utils.ipaddr('address') }}/20 dev vxlan1
     ip link set up dev vxlan1
 fi
