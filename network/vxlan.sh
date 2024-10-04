0eb9d453e4793c6dd5461a1c1495024bf2f45128
Christian Berendt <berendt@osism.tech>
2024-10-04 13:15:32 +0200

    network: do not add vxlan0 on resource nodes
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- network/vxlan.sh
+++ network/vxlan.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ $IFACE == "{{ internal_interface }}" ]]; then
+
+{% if "network" in group_names or "manager" in group_names %}
     ip link add vxlan0 type vxlan id 42 group 239.1.1.1 dstport 4789 dev {{ internal_interface }}
     ip addr add {{ '192.168.112.0/20' | ansible.utils.ipaddr('net') | ansible.utils.ipaddr(node_id) | ansible.utils.ipaddr('address') }}/20 dev vxlan0
     ip link set up dev vxlan0
+{% endif %}
 
     ip link add vxlan1 type vxlan id 23 group 239.1.1.1 dstport 4789 dev {{ internal_interface }}
     ip addr add {{ '192.168.128.0/20' | ansible.utils.ipaddr('net') | ansible.utils.ipaddr(node_id) | ansible.utils.ipaddr('address') }}/20 dev vxlan1
