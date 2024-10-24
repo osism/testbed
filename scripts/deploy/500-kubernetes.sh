5ca0ff1609302a8dfb85db74c9ef6f36d9a70adc
Christian Berendt <berendt@osism.tech>
2024-10-24 14:27:35 +0200

    Deploy & upgrade frr as part of the kubernetes deploy script
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy/500-kubernetes.sh
+++ scripts/deploy/500-kubernetes.sh
@@ -6,6 +6,7 @@ source /opt/configuration/scripts/include.sh
 source /opt/manager-vars.sh
 
 if [[ $(semver $MANAGER_VERSION 8.0.3) -ge 0 || $MANAGER_VERSION == "latest" ]]; then
+    osism apply frr
     osism apply kubernetes
     osism apply copy-kubeconfig
     osism apply k8s-dashboard
--- scripts/upgrade/500-kubernetes.sh
+++ scripts/upgrade/500-kubernetes.sh
@@ -5,6 +5,7 @@ set -e
 source /opt/configuration/scripts/include.sh
 source /opt/configuration/scripts/manager-version.sh
 
-if [[ $(semver $MANAGER_VERSION 8.0.0) -ge 0 || $MANAGER_VERSION == "latest" ]]; then
+if [[ $(semver $MANAGER_VERSION 8.0.3) -ge 0 || $MANAGER_VERSION == "latest" ]]; then
+    osism apply frr
     osism apply kubernetes
 fi
