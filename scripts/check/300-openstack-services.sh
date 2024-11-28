41288aefbd09a572b1b4174eeb0783f1959b6f4f
Christian Berendt <berendt@osism.tech>
2024-11-28 15:48:41 +0100

    openstack: add more tests
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/check/300-openstack-services.sh
+++ scripts/check/300-openstack-services.sh
@@ -2,6 +2,21 @@
 set -x
 set -e
 
+server_list() {
+    openstack --os-cloud test server list
+    openstack --os-cloud test server show test
+    openstack --os-cloud test server show test-1
+    openstack --os-cloud test server show test-2
+    openstack --os-cloud test server show test-3
+    openstack --os-cloud test server show test-4
+}
+
+compute_list() {
+    osism manage compute list testbed-node-3
+    osism manage compute list testbed-node-4
+    osism manage compute list testbed-node-5
+}
+
 source /opt/configuration/scripts/include.sh
 source /opt/configuration/scripts/manager-version.sh
 
@@ -38,20 +53,24 @@ echo "# Run OpenStack test play"
 echo
 
 osism apply --environment openstack test
-openstack --os-cloud test server list
-
-compute_list() {
-    osism manage compute list testbed-node-3
-    osism manage compute list testbed-node-4
-    osism manage compute list testbed-node-5
-}
+server_list
 
 if [[ $MANAGER_VERSION == "latest" ]]; then
     compute_list
+
+    # testbed-node-3 -> testbed-node-4
     osism manage compute migrate --yes --target testbed-node-3 testbed-node-4
+    compute_list
+
+    # testbed-node-3 -> testbed-node-5
     osism manage compute migrate --yes --target testbed-node-3 testbed-node-5
     compute_list
+
+    # testbed-node-4 -> testbed-node-3
     osism manage compute migrate --yes --target testbed-node-4 testbed-node-3
     compute_list
+
+    # testbed-node-5 -> testbed-node-4
     osism manage compute migrate --yes --target testbed-node-5 testbed-node-4
+    compute_list
 fi
