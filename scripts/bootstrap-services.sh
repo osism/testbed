4e4a669af57556d9f6fa36163993c4292a1d696b
Christian Berendt <berendt@osism.tech>
2025-01-22 20:27:32 +0100

    Fix "300-openstack-services.sh: not found" issue (#2594)
    Related to eb878af625f26f2f4e9d5055861d0a6530b37c89
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/bootstrap-services.sh
+++ scripts/bootstrap-services.sh
@@ -2,5 +2,5 @@
 set -x
 set -e
 
-sh -c '/opt/configuration/scripts/bootstrap/300-openstack-services.sh'
+sh -c '/opt/configuration/scripts/bootstrap/300-openstack.sh'
 sh -c '/opt/configuration/scripts/bootstrap/301-openstack-octavia-amhpora-image.sh'
