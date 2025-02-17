00096d885aa751b2b0955fa62d0005118ba3a55f
Christian Berendt <berendt@osism.tech>
2025-02-17 16:19:39 +0100

    Change container registry
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/set-docker-registry.sh
+++ scripts/set-docker-registry.sh
@@ -4,14 +4,14 @@ set -e
 
 source /opt/manager-vars.sh
 
-DOCKER_REGISTRY=${1:-osism.harbor.regio.digital}
+DOCKER_REGISTRY=${1:-registry.osism.tech}
 
 sed -i "s#ceph_docker_registry: .*#ceph_docker_registry: ${DOCKER_REGISTRY}#g" /opt/configuration/inventory/group_vars/all/registries.yml
 sed -i "s#docker_registry_ansible: .*#docker_registry_ansible: ${DOCKER_REGISTRY}#g" /opt/configuration/inventory/group_vars/all/registries.yml
 sed -i "s#docker_registry_kolla: .*#docker_registry_kolla: ${DOCKER_REGISTRY}#g" /opt/configuration/inventory/group_vars/all/registries.yml
 sed -i "s#docker_registry_netbox: .*#docker_registry_netbox: ${DOCKER_REGISTRY}#g" /opt/configuration/inventory/group_vars/all/registries.yml
 
-if [[ "$DOCKER_REGISTRY" == "osism.harbor.regio.digital" ]]; then
+if [[ "$DOCKER_REGISTRY" == "registry.osism.tech" ]]; then
     if [[ "$MANAGER_VERSION" == "latest" ]]; then
         sed -i "s#docker_namespace: osism#docker_namespace: kolla#" /opt/configuration/inventory/group_vars/all/kolla.yml
     else
