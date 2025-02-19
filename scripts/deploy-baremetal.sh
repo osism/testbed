85c96c20b7be9501a72636be1bcc3cf36c161e52
janhorstmann <horstmann@osism.tech>
2025-02-19 09:03:44 +0100

    Add baremetal deployment (#2607)
    Add a deployment scenario, where `testbed-resource-nodes` are used to
    provide virtual machines for baremetal deployment.
    Virtual baremetal machines are created using `tenks` ([1]), which may
    later also be used to register baremetal nodes and create matching
    flavors.
    The network `vxlan0`, which serves as an external network in ordinary
    deployments is used as a provider network conected to the virtual
    baremetal. Additionally an out-of-band network is created to manage the
    machines via virtual BMC.
    Since routing to the API network did not seem to work during PXE
    booting, ports are created for the ironic tftp and httpd services and
    connected to the control plane in a similar way to what is done in kolla
    for octavia amphora management networks ([2]). Routing to the ironic API
    during callback of the ironic python agent image works fine and is done
    via the manager node.
    
    [1]
    https://docs.openstack.org/tenks
    
    [2]
    https://opendev.org/openstack/kolla-ansible/src/branch/master/ansible/roles/octavia/tasks/hm-interface.yml
    
    Part of https://github.com/osism/issues/issues/1196
    Depends on https://github.com/osism/testbed/pull/2606
    
    Signed-off-by: Jan Horstmann <horstmann@osism.tech>

--- scripts/deploy-baremetal.sh
+++ scripts/deploy-baremetal.sh
@@ -9,14 +9,51 @@ echo
 source /opt/configuration/scripts/include.sh
 source /opt/manager-vars.sh
 
-osism apply openstackclient
+osism apply -e custom baremetal-prepare
+osism sync inventory
+osism apply network -e network_allow_service_restart=true
+
+# pull images
+sh -c '/opt/configuration/scripts/pull-images.sh'
 
 osism apply common
 osism apply loadbalancer
+osism apply openstackclient
 osism apply memcached
+osism apply redis
 osism apply mariadb
 osism apply rabbitmq
+osism apply openvswitch --limit testbed-control-nodes
+osism apply ovn --limit testbed-control-nodes
+
+# deploy ceph services
+if [[ $CEPH_STACK == "ceph-ansible" ]]; then
+    sh -c '/opt/configuration/scripts/deploy/100-ceph-with-ansible.sh'
+elif [[ $CEPH_STACK == "rook" ]]; then
+    sh -c '/opt/configuration/scripts/deploy/100-ceph-with-rook.sh'
+fi
+
 osism apply keystone
+osism apply placement
+osism apply neutron
+
+osism apply -e custom baremetal-bootstrap
+osism sync facts
+osism apply ironic
+
+osism apply glance
+osism apply nova --limit testbed-control-nodes
+
+osism apply tenks -e ireallymeanit=yes -e tenks_override_file=/opt/configuration/environments/custom/files/baremetal-tenks-override.yml
+
+osism apply designate
+osism apply kolla-ceph-rgw
 osism apply horizon
+osism apply skyline
+osism apply cinder
+osism apply barbican
+osism apply octavia
+osism apply ceilometer
+osism apply aodh
 
 osism apply homer
