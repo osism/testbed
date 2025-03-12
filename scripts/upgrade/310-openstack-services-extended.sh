dcee94dd87ff9aa413e314407cf56992588f6378
Christian Berendt <berendt@osism.tech>
2025-03-12 19:41:06 +0100

    Do not deploy heat (#2631)
    We don't usually use heat anywhere. We don't test it. Let's stop
    deloying it in the testbed.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/upgrade/310-openstack-services-extended.sh
+++ scripts/upgrade/310-openstack-services-extended.sh
@@ -5,5 +5,4 @@ source /opt/configuration/scripts/include.sh
 source /opt/configuration/scripts/manager-version.sh
 
 osism apply -a upgrade gnocchi
-osism apply -a upgrade heat
 osism apply -a upgrade manila
