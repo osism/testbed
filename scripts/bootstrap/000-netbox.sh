7abef6a237b0e4af67da7115ffe3eaeaaa147408
Christian Berendt <berendt@osism.tech>
2025-03-14 21:01:55 +0100

    Use new osism netbox manage in 000-netbox.sh script (#2635)
    Related to osism/issues#1223
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/bootstrap/000-netbox.sh
+++ scripts/bootstrap/000-netbox.sh
@@ -2,11 +2,4 @@
 set -x
 set -e
 
-osism netbox import
-osism netbox init
-osism netbox manage 1000
-osism netbox connect 1000 --state a
-
-osism netbox disable --no-wait testbed-switch-0
-osism netbox disable --no-wait testbed-switch-1
-osism netbox disable --no-wait testbed-switch-2
+osism manage netbox
