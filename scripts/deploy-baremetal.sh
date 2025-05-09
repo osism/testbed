6d753e2d4f27ae162015368c88560063f7ebdc3e
Christian Berendt <berendt@osism.tech>
2025-05-09 14:57:37 +0200

    Minify the baremetal deployment (#2676)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/deploy-baremetal.sh
+++ scripts/deploy-baremetal.sh
@@ -13,6 +13,11 @@ sed -i 's/enable_ironic: "no"/enable_ironic: "yes"/' /opt/configuration/environm
 sed -i 's/glance_backend_ceph: "yes"/glance_backend_ceph: "no"/' /opt/configuration/environments/kolla/configuration.yml
 sed -i 's/glance_backend_file: "no"/glance_backend_file: "yes"/' /opt/configuration/environments/kolla/configuration.yml
 
+echo 'enable_cinder: "no"' >> /opt/configuration/environments/kolla/configuration.yml
+echo 'enable_horizon: "no"' >> /opt/configuration/environments/kolla/configuration.yml
+echo 'enable_neutron: "no"' >> /opt/configuration/environments/kolla/configuration.yml
+echo 'enable_nova: "no"' >> /opt/configuration/environments/kolla/configuration.yml
+
 osism apply -e custom baremetal-prepare
 osism sync inventory
 osism apply network -e network_allow_service_restart=true
@@ -27,27 +32,16 @@ osism apply memcached
 osism apply redis
 osism apply mariadb
 osism apply rabbitmq
-osism apply openvswitch --limit testbed-control-nodes
-osism apply ovn --limit testbed-control-nodes
 
 osism apply keystone
-osism apply placement
-osism apply neutron
+osism apply glance
 
 osism apply -e custom baremetal-bootstrap
 osism sync facts
 osism apply ironic
 
-osism apply glance
-osism apply nova --limit testbed-control-nodes
-
 osism apply tenks -e ireallymeanit=yes -e tenks_override_file=/opt/configuration/environments/custom/files/baremetal-tenks-override.yml
 osism apply -e custom baremetal-netbox
 sh -c '/opt/configuration/scripts/bootstrap/000-netbox.sh'
-
-osism apply designate
-osism apply horizon
-osism apply barbican
-osism apply octavia
 
 osism apply homer
