2450bb78d0208d160968f5518ed864e4350395c3
Christian Berendt <berendt@osism.tech>
2025-07-05 19:49:30 +0200

    Make 172.31.252.0/23 (Cilium BGP CIDR) usable via wireguard
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- network/iptables.sh
+++ network/iptables.sh
@@ -4,4 +4,7 @@ if [[ $IFACE == "{{ internal_interface }}" ]]; then
     iptables -A FORWARD -i {{ internal_interface }} -j ACCEPT
     iptables -A FORWARD -o {{ internal_interface }} -j ACCEPT
     iptables -t nat -A POSTROUTING -o {{ internal_interface }} -j MASQUERADE
+
+    iptables -t nat -A POSTROUTING -s 192.168.48.0/24 -d 172.31.252.0/23 -j MASQUERADE
+    iptables -t nat -A POSTROUTING -s 192.168.48.0/24 -d 172.31.252.0/23 -j SNAT --to-source 192.168.48.1
 fi
