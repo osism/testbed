c96ac04bf32995c88ae35aeff8a13415dc61afc4
Christian Berendt <berendt@osism.tech>
2025-12-10 11:59:01 +0100

    Remove thanos-sidecar service (#2809)
    The service is currently not used in any known deployments and
    can therefore be removed from regular testing.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/upgrade/400-monitoring.sh
+++ scripts/upgrade/400-monitoring.sh
@@ -6,7 +6,3 @@ source /opt/configuration/scripts/manager-version.sh
 
 osism apply -a upgrade prometheus
 osism apply -a upgrade grafana
-
-if [[ $(semver $MANAGER_VERSION 7.0.0) -ge 0 || $MANAGER_VERSION == "latest" ]]; then
-    osism apply thanos_sidecar
-fi
